d5bdb8c47759406dac45624f2979448795ee20e5
CDDelta <ngjunyen@gmail.com>
2020-08-13 19:18:16 +1000

    fix build

--- lib/repositories/database/ffi.dart
+++ lib/repositories/database/ffi.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
+import 'package:moor/ffi.dart';
 import 'package:moor/moor.dart';
-import 'package:moor_ffi/moor_ffi.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
