bc2f9bf595df76df8419d7d3e0316e9e501fab5b
CDDelta <ngjunyen@gmail.com>
2020-10-13 14:05:14 +1100

    merge FolderRenameCubit and FileRenameCubit... into FsEntryRenameCubit

---
+++ lib/blocs/fs_entry_rename/fs_entry_rename_cubit.dart
@@ -0,0 +1,95 @@
+import 'package:ardrive/blocs/blocs.dart';
+import 'package:ardrive/models/models.dart';
+import 'package:ardrive/services/services.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+part 'fs_entry_rename_state.dart';
+
+class FsEntryRenameCubit extends Cubit<FsEntryRenameState> {
+  final form = FormGroup({
+    'name': FormControl(validators: [Validators.required]),
+  });
+
+  final String driveId;
+  final String folderId;
+  final String fileId;
+
+  final ArweaveService _arweave;
+  final DriveDao _driveDao;
+  final ProfileBloc _profileBloc;
+
+  bool get _isRenamingFolder => folderId != null;
+
+  FsEntryRenameCubit({
+    @required this.driveId,
+    this.folderId,
+    this.fileId,
+    @required ArweaveService arweave,
+    @required DriveDao driveDao,
+    @required ProfileBloc profileBloc,
+  })  : _arweave = arweave,
+        _driveDao = driveDao,
+        _profileBloc = profileBloc,
+        assert(folderId != null || fileId != null),
+        super(FsEntryRenameInitializing(isRenamingFolder: folderId != null)) {
+    () async {
+      final name = _isRenamingFolder
+          ? await _driveDao.getFolderNameById(driveId, folderId)
+          : await _driveDao.getFileNameById(driveId, fileId);
+
+      form.control('name').value = name;
+      emit(FsEntryRenameInitialized(isRenamingFolder: _isRenamingFolder));
+    }();
+  }
+
+  Future<void> submit() async {
+    if (form.invalid) {
+      return;
+    }
+
+    final String newName = form.control('name').value;
+    final profile = _profileBloc.state as ProfileLoaded;
+    final driveKey = await _driveDao.getDriveKey(driveId, profile.cipherKey);
+
+    if (_isRenamingFolder) {
+      emit(FolderEntryRenameInProgress());
+
+      await _driveDao.transaction(() async {
+        var folder = await _driveDao.getFolderById(driveId, folderId);
+        folder = folder.copyWith(name: newName);
+
+        final folderTx = await _arweave.prepareEntityTx(
+          folder.asEntity(),
+          profile.wallet,
+          driveKey,
+        );
+
+        await _arweave.postTx(folderTx);
+        await _driveDao.updateFolder(folder);
+      });
+
+      emit(FolderEntryRenameSuccess());
+    } else {
+      emit(FileEntryRenameInProgress());
+
+      await _driveDao.transaction(() async {
+        var file = await _driveDao.getFileById(driveId, fileId);
+        file = file.copyWith(name: newName);
+
+        final fileTx = await _arweave.prepareEntityTx(
+          file.asEntity(),
+          profile.wallet,
+          driveKey,
+        );
+
+        await _arweave.postTx(fileTx);
+        await _driveDao.updateFile(file);
+      });
+
+      emit(FileEntryRenameSuccess());
+    }
+  }
+}
