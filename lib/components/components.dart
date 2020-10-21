07d516f764f1ad69d9af1564cfbd24e0d040df8e
CDDelta <ngjunyen@gmail.com>
2020-10-21 16:30:13 +1100

    update drawer list colors

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,5 +1,5 @@
 export 'app_dialog.dart';
-export 'app_drawer.dart';
+export 'app_drawer/app_drawer.dart';
 export 'confirmation_dialog.dart';
 export 'drive_attach_form.dart';
 export 'drive_create_form.dart';
