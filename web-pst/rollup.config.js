ce58d7a54ef70accedacf8ae1680431649d93797
CDDelta <ngjunyen@gmail.com>
2020-10-27 21:41:45 +1100

    add PST address script

---
+++ web-pst/rollup.config.js
@@ -0,0 +1,34 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+const commonOutputOpts = {
+  format: 'iife',
+  name: 'pst',
+  globals: {
+    'util': 'window'
+  },
+}
+
+export default {
+  input: 'src/index.ts',
+  output:  [
+    {
+      ...commonOutputOpts,
+      file: 'dist/pst.js',
+    },
+    {
+      ...commonOutputOpts,
+      file: '../web/pst.min.js',
+      plugins: [terser()],
+    }
+  ],
+  plugins: [
+    json(),
+    nodeResolve({ browser: true }),
+    commonjs(),
+    typescript()
+  ],
+};
---
+++ web-pst/src/index.ts
@@ -0,0 +1,18 @@
+import Arweave from 'arweave';
+// Import smartweave functions individually to aid Rollup treeshaking.
+import { readContract } from 'smartweave/lib/contract-read';
+import { selectWeightedPstHolder } from 'smartweave/lib/weighted-pst-holder';
+
+// Initialise an Arweave client using the default options.
+const arweave = Arweave.init({});
+
+const pstContractId = '-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ';
+
+// Read the PST contract state and cache it for the life of the script.
+const pstContractStateRead = readContract(arweave, pstContractId);
+
+export async function getWeightedPstHolder(): Promise<String> {
+  // Select a PST holder from the cached PST contract state.
+  const contractState = await pstContractStateRead;
+  return selectWeightedPstHolder(contractState.balances);
+}
