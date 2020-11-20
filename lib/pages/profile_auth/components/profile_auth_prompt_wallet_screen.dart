92f623d035e20e4cbc8a092fc3173c93c3afb7f7
CDDelta <14818946+CDDelta@users.noreply.github.com>
2020-11-20 23:25:37 +1100

    Update onboarding flow (#53)
    * update onboarding flow
    
    * fix onboarding step jitter

--- lib/pages/profile_auth/components/profile_auth_prompt_wallet_screen.dart
+++ lib/pages/profile_auth/components/profile_auth_prompt_wallet_screen.dart
@@ -12,43 +12,41 @@ class ProfileAuthPromptWalletScreen extends StatelessWidget {
   Widget build(BuildContext context) => ProfileAuthShell(
         illustration: Image.asset(
           R.images.profile.profileWelcome,
-          fit: BoxFit.scaleDown,
+          fit: BoxFit.contain,
         ),
-        content: FractionallySizedBox(
-          widthFactor: 0.5,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'WELCOME TO',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Container(height: 32),
-              Text(
-                'Your private and secure, decentralized, pay-as-you-go, censorship-resistant and permanent hard drive.',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Container(height: 32),
-              ElevatedButton(
-                child: Text('SELECT WALLET'),
-                onPressed: () => _pickWallet(context),
-              ),
-              Container(height: 16),
-              Link(
-                uri: Uri.parse('https://tokens.arweave.org'),
-                target: LinkTarget.blank,
-                builder: (context, followLink) => TextButton(
-                  onPressed: followLink,
-                  child: Text(
-                    'Don\'t have a wallet? Get one here!',
-                    textAlign: TextAlign.center,
-                  ),
+        contentWidthFactor: 0.5,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'WELCOME TO',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Container(height: 32),
+            Text(
+              'Your private and secure, decentralized, pay-as-you-go, censorship-resistant and permanent hard drive.',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Container(height: 32),
+            ElevatedButton(
+              child: Text('SELECT WALLET'),
+              onPressed: () => _pickWallet(context),
+            ),
+            Container(height: 16),
+            Link(
+              uri: Uri.parse('https://tokens.arweave.org'),
+              target: LinkTarget.blank,
+              builder: (context, followLink) => TextButton(
+                onPressed: followLink,
+                child: Text(
+                  'Don\'t have a wallet? Get one here!',
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
 
--- lib/pages/profile_auth/components/profile_auth_shell.dart
+++ lib/pages/profile_auth/components/profile_auth_shell.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 
 class ProfileAuthShell extends StatelessWidget {
   final Widget illustration;
-  final Widget content;
 
-  ProfileAuthShell({this.illustration, this.content});
+  final double contentWidthFactor;
+  final Widget content;
+  final Widget contentFooter;
+
+  ProfileAuthShell(
+      {this.illustration,
+      this.contentWidthFactor,
+      this.content,
+      this.contentFooter});
 
   @override
   Widget build(BuildContext context) => Material(
@@ -14,30 +21,39 @@ class ProfileAuthShell extends StatelessWidget {
             Expanded(
               child: Container(
                 color: kDarkSurfaceColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Center(
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: 512),
-                      child: illustration,
-                    ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: illustration,
                   ),
                 ),
               ),
             ),
             Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/brand/logo-vert-no-subtitle.png',
-                    height: 126,
-                    fit: BoxFit.contain,
-                  ),
-                  Container(height: 16),
-                  content,
-                ],
+              child: FractionallySizedBox(
+                widthFactor: contentWidthFactor,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      constraints: const BoxConstraints(minHeight: 512),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/brand/logo-vert-no-subtitle.png',
+                            height: 126,
+                            fit: BoxFit.contain,
+                          ),
+                          Container(height: 32),
+                          content,
+                        ],
+                      ),
+                    ),
+                    if (contentFooter != null) contentFooter,
+                  ],
+                ),
               ),
             ),
           ],
