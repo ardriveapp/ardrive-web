e012a6df068259bbba0fac29bd19fa17c661c42e
CDDelta <ngjunyen@gmail.com>
2020-11-20 21:53:13 +1100

    fix DrivesCubit widget tree location

--- lib/pages/app_router_delegate.dart
+++ lib/pages/app_router_delegate.dart
@@ -28,54 +28,46 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
           if (state is! ProfileLoaded) {
             shell = ProfileAuthPage();
           } else {
-            shell = BlocProvider(
-              create: (context) => DrivesCubit(
-                initialSelectedDriveId: driveId,
-                profileCubit: context.read<ProfileCubit>(),
-                drivesDao: context.read<DrivesDao>(),
-              ),
-              child: BlocConsumer<DrivesCubit, DrivesState>(
-                listener: (context, state) {
-                  if (state is DrivesLoadSuccess) {
-                    navigateToDriveDetailPage(state.selectedDriveId);
-                  }
-                },
-                builder: (context, state) {
-                  Widget shellPage;
-                  if (state is DrivesLoadSuccess) {
-                    if (state.hasNoDrives) {
-                      shellPage = Center(
-                        child: Text(
-                          'You have no personal or attached drives.\nClick the "new" button to add some!',
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                      );
-                    } else {
-                      shellPage =
-                          DriveDetailPage(driveId: state.selectedDriveId);
-                    }
+            shell = BlocConsumer<DrivesCubit, DrivesState>(
+              listener: (context, state) {
+                if (state is DrivesLoadSuccess) {
+                  navigateToDriveDetailPage(state.selectedDriveId);
+                }
+              },
+              builder: (context, state) {
+                Widget shellPage;
+                if (state is DrivesLoadSuccess) {
+                  if (state.hasNoDrives) {
+                    shellPage = Center(
+                      child: Text(
+                        'You have no personal or attached drives.\nClick the "new" button to add some!',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    );
                   } else {
-                    shellPage = Container();
+                    shellPage = DriveDetailPage(driveId: state.selectedDriveId);
                   }
+                } else {
+                  shellPage = Container();
+                }
 
-                  return BlocProvider(
-                    key: ValueKey(driveId),
-                    create: (context) => DriveDetailCubit(
-                      driveId: driveId,
-                      initialFolderId: driveFolderId,
-                      profileCubit: context.read<ProfileCubit>(),
-                      driveDao: context.read<DriveDao>(),
-                      config: context.read<AppConfig>(),
-                    ),
-                    child: AppShell(page: shellPage),
-                  );
-                },
-              ),
+                return BlocProvider(
+                  key: ValueKey(driveId),
+                  create: (context) => DriveDetailCubit(
+                    driveId: driveId,
+                    initialFolderId: driveFolderId,
+                    profileCubit: context.read<ProfileCubit>(),
+                    driveDao: context.read<DriveDao>(),
+                    config: context.read<AppConfig>(),
+                  ),
+                  child: AppShell(page: shellPage),
+                );
+              },
             );
           }
 
-          return Navigator(
+          final navigator = Navigator(
             key: navigatorKey,
             pages: [
               MaterialPage(
@@ -92,6 +84,19 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
               return true;
             },
           );
+
+          if (state is! ProfileLoaded) {
+            return navigator;
+          } else {
+            return BlocProvider(
+              create: (context) => DrivesCubit(
+                initialSelectedDriveId: driveId,
+                profileCubit: context.read<ProfileCubit>(),
+                drivesDao: context.read<DrivesDao>(),
+              ),
+              child: navigator,
+            );
+          }
         },
       );
 
