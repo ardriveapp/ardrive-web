a865cd0d59e3f17fe63f42890cea50803b78e269
CDDelta <ngjunyen@gmail.com>
2020-11-27 18:23:31 +1100

    bump the database schema and force a reset

--- lib/models/database/database.dart
+++ lib/models/database/database.dart
@@ -20,5 +20,22 @@ class Database extends _$Database {
   Database([QueryExecutor e]) : super(e ?? openConnection());
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
+
+  @override
+  MigrationStrategy get migration => MigrationStrategy(
+        onCreate: (Migrator m) {
+          return m.createAll();
+        },
+        onUpgrade: (Migrator m, int from, int to) async {
+          if (from == 1) {
+            // Reset the database.
+            for (final table in allTables) {
+              await m.deleteTable(table.actualTableName);
+            }
+
+            await m.createAll();
+          }
+        },
+      );
 }
