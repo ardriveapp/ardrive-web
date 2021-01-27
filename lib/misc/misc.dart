c21a6430e0f7cb016df47b1525148174744e6eb2
CDDelta <me@cddelta.dev>
2021-01-27 11:37:45 +1100

    stash

--- lib/misc/misc.dart
+++ lib/misc/misc.dart
@@ -1,5 +1,5 @@
 export 'resources.dart';
 
 const kDriveNameRegex = kFolderNameRegex;
-const kFolderNameRegex = r'[^\/\\\<\>\:\"\?]+[^\/\\\<\>\:\"\?\.]$';
-const kFileNameRegex = r'^[^\\\/]+[^\s.\/\\]$';
+const kFolderNameRegex = r'[^\/\\\<\>\:\"\?]+(?<!\.)$';
+const kFileNameRegex = r'[^\\\/]+(?<!\.)$';
