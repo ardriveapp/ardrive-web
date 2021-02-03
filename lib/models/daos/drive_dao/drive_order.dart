5bca31e098a1a0951a6715d636d9c6b6d0c49c9b
CDDelta <me@cddelta.dev>
2021-02-03 11:47:38 +1100

    refactor: migrate more queries to use Moor files

--- lib/models/daos/drive_dao/drive_order.dart
+++ lib/models/daos/drive_dao/drive_order.dart
@@ -5,3 +5,35 @@ enum DriveOrder {
   size,
   lastUpdated,
 }
+
+OrderBy enumToFolderOrderByClause(FolderEntries table, DriveOrder orderBy,
+    [OrderingMode orderingMode = OrderingMode.asc]) {
+  switch (orderBy) {
+    // Folders have no size or proper last updated time to be sorted by
+    // so we just sort them ascendingly by name.
+    case DriveOrder.lastUpdated:
+    case DriveOrder.size:
+      return OrderBy(
+          [OrderingTerm(expression: table.name, mode: OrderingMode.asc)]);
+    case DriveOrder.name:
+    default:
+      return OrderBy(
+          [OrderingTerm(expression: table.name, mode: orderingMode)]);
+  }
+}
+
+OrderBy enumToFileOrderByClause(FileEntries table, DriveOrder orderBy,
+    [OrderingMode orderingMode = OrderingMode.asc]) {
+  switch (orderBy) {
+    case DriveOrder.lastUpdated:
+      return OrderBy(
+          [OrderingTerm(expression: table.lastUpdated, mode: orderingMode)]);
+    case DriveOrder.size:
+      return OrderBy(
+          [OrderingTerm(expression: table.size, mode: orderingMode)]);
+    case DriveOrder.name:
+    default:
+      return OrderBy(
+          [OrderingTerm(expression: table.name, mode: orderingMode)]);
+  }
+}
