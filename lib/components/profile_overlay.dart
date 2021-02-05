90240fbf8179d92bb94d0c82f46ca6e0ec7f530e
Javed Hussain <jdhn42@gmail.com>
2021-02-05 09:51:23 -0500

    fix: tweaked alignment and size of profile overlay

--- lib/components/profile_overlay.dart
+++ lib/components/profile_overlay.dart
@@ -16,7 +16,7 @@ class ProfileOverlay extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.fromLTRB(16, 12, 16, 16),
               child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 448),
+                constraints: const BoxConstraints(maxWidth: kMediumDialogWidth),
                 child: BlocBuilder<ProfileCubit, ProfileState>(
                   builder: (context, state) => state is ProfileLoggedIn
                       ? ListTile(
