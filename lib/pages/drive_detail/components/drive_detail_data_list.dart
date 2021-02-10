65b6edc9a7eb4f3bae465284c8caade7bb9f9485
Javed Hussein <jdhn42@gmail.com>
2021-02-09 19:56:22 -0500

    refactor: list dividers now done with intersperse

--- lib/pages/drive_detail/components/drive_detail_data_list.dart
+++ lib/pages/drive_detail/components/drive_detail_data_list.dart
@@ -3,39 +3,43 @@ part of '../drive_detail_page.dart';
 Widget _buildDataList(BuildContext context, DriveDetailLoadSuccess state) =>
     ListView(
       children: [
-        ...state.currentFolder.subfolders.map(
-          (folder) => _buildFolderListTile(
-            context: context,
-            folder: folder,
-            selected: folder.id == state.selectedItemId,
-            onPressed: () {
-              final bloc = context.read<DriveDetailCubit>();
-              if (folder.id == state.selectedItemId) {
-                bloc.openFolder(path: folder.path);
-              } else {
-                bloc.selectItem(
-                  folder.id,
-                  isFolder: true,
-                );
-              }
-            },
-          ),
-        ),
-        ...state.currentFolder.files.map(
-          (file) => _buildFileListTile(
-            context: context,
-            file: file,
-            selected: file.id == state.selectedItemId,
-            onPressed: () async {
-              final bloc = context.read<DriveDetailCubit>();
-              if (file.id == state.selectedItemId) {
-                bloc.toggleSelectedItemDetails();
-              } else {
-                await bloc.selectItem(file.id);
-              }
-            },
-          ),
-        ),
+        ...state.currentFolder.subfolders
+            .map(
+              (folder) => _buildFolderListTile(
+                context: context,
+                folder: folder,
+                selected: folder.id == state.selectedItemId,
+                onPressed: () {
+                  final bloc = context.read<DriveDetailCubit>();
+                  if (folder.id == state.selectedItemId) {
+                    bloc.openFolder(path: folder.path);
+                  } else {
+                    bloc.selectItem(
+                      folder.id,
+                      isFolder: true,
+                    );
+                  }
+                },
+              ),
+            )
+            .intersperse(Divider()),
+        ...state.currentFolder.files
+            .map(
+              (file) => _buildFileListTile(
+                context: context,
+                file: file,
+                selected: file.id == state.selectedItemId,
+                onPressed: () async {
+                  final bloc = context.read<DriveDetailCubit>();
+                  if (file.id == state.selectedItemId) {
+                    bloc.toggleSelectedItemDetails();
+                  } else {
+                    await bloc.selectItem(file.id);
+                  }
+                },
+              ),
+            )
+            .intersperseOuter(Divider()),
       ],
     );
 
@@ -45,19 +49,14 @@ Widget _buildFolderListTile({
   bool selected = false,
   Function onPressed,
 }) =>
-    Column(
-      children: [
-        ListTile(
-          onTap: () => onPressed(),
-          selected: selected,
-          leading: Padding(
-            padding: const EdgeInsetsDirectional.only(end: 8.0),
-            child: const Icon(Icons.folder),
-          ),
-          title: Text(folder.name),
-        ),
-        Divider(),
-      ],
+    ListTile(
+      onTap: () => onPressed(),
+      selected: selected,
+      leading: Padding(
+        padding: const EdgeInsetsDirectional.only(end: 8.0),
+        child: const Icon(Icons.folder),
+      ),
+      title: Text(folder.name),
     );
 
 Widget _buildFileListTile({
@@ -66,26 +65,21 @@ Widget _buildFileListTile({
   bool selected = false,
   Function onPressed,
 }) =>
-    Column(
-      children: [
-        ListTile(
-          onTap: () => onPressed(),
-          selected: selected,
-          leading: Padding(
-            padding: const EdgeInsetsDirectional.only(end: 8.0),
-            child: _buildFileIcon(
-              fileStatusFromTransactions(file.metadataTx, file.dataTx),
-              file.dataContentType,
-            ),
-          ),
-          title: Text(file.name),
-          subtitle: Text(
-            'Last Modified ' +
-                (file.lastUpdated.difference(DateTime.now()).inDays > 3
-                    ? format(file.lastUpdated)
-                    : yMMdDateFormatter.format(file.lastUpdated)),
-          ),
+    ListTile(
+      onTap: () => onPressed(),
+      selected: selected,
+      leading: Padding(
+        padding: const EdgeInsetsDirectional.only(end: 8.0),
+        child: _buildFileIcon(
+          fileStatusFromTransactions(file.metadataTx, file.dataTx),
+          file.dataContentType,
         ),
-        Divider()
-      ],
+      ),
+      title: Text(file.name),
+      subtitle: Text(
+        'Last Modified ' +
+            (file.lastUpdated.difference(DateTime.now()).inDays > 3
+                ? format(file.lastUpdated)
+                : yMMdDateFormatter.format(file.lastUpdated)),
+      ),
     );
