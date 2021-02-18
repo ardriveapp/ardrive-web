5fce65222dbf1815b30652a7dff390e00c039233
Javed Hussein <jdhn42@gmail.com>
2021-02-17 19:53:34 -0500

    fix: shared file page not showing (#95)
    * nit: removed try catch in shared file page
    
    * fix: added back navigation to shared file page

--- lib/pages/shared_file/shared_file_page.dart
+++ lib/pages/shared_file/shared_file_page.dart
@@ -12,74 +12,67 @@ import 'package:url_launcher/link.dart';
 class SharedFilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    try {
-      return Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxWidth: kMediumDialogWidth,
-            minWidth: kMediumDialogWidth,
-            minHeight: 256,
-          ),
-          child: Card(
-            elevation: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: BlocBuilder<SharedFileCubit, SharedFileState>(
-                builder: (context, state) => Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      R.images.brand.logoVerticalNoSubtitle,
-                      height: 96,
-                      fit: BoxFit.contain,
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: kMediumDialogWidth,
+          minWidth: kMediumDialogWidth,
+          minHeight: 256,
+        ),
+        child: Card(
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: BlocBuilder<SharedFileCubit, SharedFileState>(
+              builder: (context, state) => Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    R.images.brand.logoVerticalNoSubtitle,
+                    height: 96,
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(height: 32),
+                  if (state is SharedFileLoadInProgress)
+                    const CircularProgressIndicator()
+                  else if (state is SharedFileLoadSuccess) ...{
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: const Icon(Icons.text_snippet),
+                      title: Text(state.file.name),
+                      subtitle: Text(filesize(state.file.size)),
                     ),
-                    const SizedBox(height: 32),
-                    if (state is SharedFileLoadInProgress)
-                      const CircularProgressIndicator()
-                    else if (state is SharedFileLoadSuccess) ...{
-                      ListTile(
-                        contentPadding: EdgeInsets.zero,
-                        leading: const Icon(Icons.text_snippet),
-                        title: Text(state.file.name),
-                        subtitle: Text(filesize(state.file.size)),
+                    const SizedBox(height: 24),
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.file_download),
+                      label: Text('Download'),
+                      onPressed: () => promptToDownloadSharedFile(
+                        context: context,
+                        fileId: state.file.id,
+                        fileKey: state.fileKey,
                       ),
-                      const SizedBox(height: 24),
-                      ElevatedButton.icon(
-                        icon: Icon(Icons.file_download),
-                        label: Text('Download'),
-                        onPressed: () => promptToDownloadSharedFile(
-                          context: context,
-                          fileId: state.file.id,
-                          fileKey: state.fileKey,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      _buildReturnToAppLink(),
-                    } else if (state is SharedFileNotFound) ...{
-                      const Icon(Icons.error_outline, size: 36),
-                      const SizedBox(height: 16),
-                      Text(
-                        'The specified file does not exist.',
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 24),
-                      _buildReturnToAppLink(),
-                    }
-                  ],
-                ),
+                    ),
+                    const SizedBox(height: 16),
+                    _buildReturnToAppLink(),
+                  } else if (state is SharedFileNotFound) ...{
+                    const Icon(Icons.error_outline, size: 36),
+                    const SizedBox(height: 16),
+                    Text(
+                      'The specified file does not exist.',
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 24),
+                    _buildReturnToAppLink(),
+                  }
+                ],
               ),
             ),
           ),
         ),
-      );
-    } catch (e) {
-      return Text(
-        e.toString(),
-        style: Theme.of(context).textTheme.headline6,
-      );
-    }
+      ),
+    );
   }
 
   Widget _buildReturnToAppLink() => Link(
