1559f2abc5505a7d17c859b385094029141a0089
jdaev <jdhn42@gmail.com>
2021-04-22 10:09:02 -0400

    feat: add scrollbar to appbar

--- lib/components/app_drawer/app_drawer.dart
+++ lib/components/app_drawer/app_drawer.dart
@@ -33,62 +33,64 @@ class AppDrawer extends StatelessWidget {
                         _buildDriveActionsButton(context, state),
                         if (state is DrivesLoadSuccess)
                           Expanded(
-                            child: ListView(
-                              children: [
-                                if (state.userDrives.isNotEmpty ||
-                                    state.sharedDrives.isEmpty) ...{
-                                  ListTile(
-                                    dense: true,
-                                    title: Text(
-                                      'PERSONAL DRIVES',
-                                      textAlign: TextAlign.start,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .caption
-                                          .copyWith(
-                                              color: ListTileTheme.of(context)
-                                                  .textColor),
+                            child: Scrollbar(
+                              child: ListView(
+                                children: [
+                                  if (state.userDrives.isNotEmpty ||
+                                      state.sharedDrives.isEmpty) ...{
+                                    ListTile(
+                                      dense: true,
+                                      title: Text(
+                                        'PERSONAL DRIVES',
+                                        textAlign: TextAlign.start,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .caption
+                                            .copyWith(
+                                                color: ListTileTheme.of(context)
+                                                    .textColor),
+                                      ),
+                                      trailing: _buildSyncButton(),
                                     ),
-                                    trailing: _buildSyncButton(),
-                                  ),
-                                  ...state.userDrives.map(
-                                    (d) => DriveListTile(
-                                      drive: d,
-                                      selected: state.selectedDriveId == d.id,
-                                      onPressed: () => context
-                                          .read<DrivesCubit>()
-                                          .selectDrive(d.id),
+                                    ...state.userDrives.map(
+                                      (d) => DriveListTile(
+                                        drive: d,
+                                        selected: state.selectedDriveId == d.id,
+                                        onPressed: () => context
+                                            .read<DrivesCubit>()
+                                            .selectDrive(d.id),
+                                      ),
                                     ),
-                                  ),
-                                },
-                                if (state.sharedDrives.isNotEmpty) ...{
-                                  ListTile(
-                                    dense: true,
-                                    title: Text(
-                                      'SHARED DRIVES',
-                                      textAlign: TextAlign.start,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .caption
-                                          .copyWith(
-                                              color: ListTileTheme.of(context)
-                                                  .textColor),
+                                  },
+                                  if (state.sharedDrives.isNotEmpty) ...{
+                                    ListTile(
+                                      dense: true,
+                                      title: Text(
+                                        'SHARED DRIVES',
+                                        textAlign: TextAlign.start,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .caption
+                                            .copyWith(
+                                                color: ListTileTheme.of(context)
+                                                    .textColor),
+                                      ),
+                                      trailing: state.userDrives.isEmpty
+                                          ? _buildSyncButton()
+                                          : null,
                                     ),
-                                    trailing: state.userDrives.isEmpty
-                                        ? _buildSyncButton()
-                                        : null,
-                                  ),
-                                  ...state.sharedDrives.map(
-                                    (d) => DriveListTile(
-                                      drive: d,
-                                      selected: state.selectedDriveId == d.id,
-                                      onPressed: () => context
-                                          .read<DrivesCubit>()
-                                          .selectDrive(d.id),
+                                    ...state.sharedDrives.map(
+                                      (d) => DriveListTile(
+                                        drive: d,
+                                        selected: state.selectedDriveId == d.id,
+                                        onPressed: () => context
+                                            .read<DrivesCubit>()
+                                            .selectDrive(d.id),
+                                      ),
                                     ),
-                                  ),
-                                }
-                              ],
+                                  }
+                                ],
+                              ),
                             ),
                           ),
                       ],
