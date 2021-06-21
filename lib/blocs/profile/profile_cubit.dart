57b838134d1797e82f814a3b3c343984d2114cf4
jdaev <jdhn42@gmail.com>
2021-06-21 12:01:29 -0400

    fix: reorder profile null check rm-316

--- lib/blocs/profile/profile_cubit.dart
+++ lib/blocs/profile/profile_cubit.dart
@@ -71,13 +71,12 @@ class ProfileCubit extends Cubit<ProfileState> {
 
   Future<void> checkForWalletMismatch() async {
     final profile = await _profileDao.defaultProfile().getSingleOrNull();
-    if (!(await arconnect.checkPermissions())) {
-      await logoutProfile();
-      return;
-    }
-    if (profile.profileType == ProfileType.ArConnect.index) {
-      if (profile != null &&
-          profile.profileType == ProfileType.ArConnect.index) {
+    if (profile != null) {
+      if (!(await arconnect.checkPermissions())) {
+        await logoutProfile();
+        return;
+      }
+      if (profile.profileType == ProfileType.ArConnect.index) {
         final currentPublicKey = await arconnect.getPublicKey();
         final savedPublicKey =
             (await _db.profileDao.defaultProfile().getSingleOrNull())
