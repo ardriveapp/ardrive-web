e0ae7b21dedd4a7b8ce24e506d07fd8df42273e3
Javed Hussein <jdhn42@gmail.com>
2021-09-17 08:46:36 -0400

    task: regenerate ios files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
 import package_info_plus_macos
 import path_provider_macos
+import sqlite3_flutter_libs
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,9 +1,8 @@
 import 'package:ardrive/main.dart' as app;
-import 'package:flutter_driver/driver_extension.dart';
 
 void main() {
   // This line enables the extension.
-  enableFlutterDriverExtension();
+  // enableFlutterDriverExtension();
 
   // Call the `main()` function of the app, or call `runApp` with
   // any widget you are interested in testing.
