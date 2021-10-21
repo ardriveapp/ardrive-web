667230aabb0f6f3d23655b097259de661546d6d2
jdaev <jdhn42@gmail.com>
2021-10-21 16:15:34 -0400

    fix: Fix folder name overlap PE-22

--- lib/pages/drive_detail/components/drive_detail_data_table.dart
+++ lib/pages/drive_detail/components/drive_detail_data_table.dart
@@ -95,7 +95,14 @@ DataRow _buildFolderRow({
                 padding: const EdgeInsetsDirectional.only(end: 8.0),
                 child: const Icon(Icons.folder),
               ),
-              Text(folder.name),
+              Container(
+                width: MediaQuery.of(context).size.width / 1.7,
+                child: Text(
+                  folder.name,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
             ],
           ),
         ),
