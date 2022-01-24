133796c5832bd84ae64db0bdf84bbe8a9a6b5970
Javed Hussein <jdhn42@gmail.com>
2022-01-24 15:04:41 -0500

    chore: update database-checker

--- database_checker.sh
+++ database_checker.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 if [ $(git status --porcelain ./lib/models/tables | wc -l) -eq "0" ]; then
     echo "  🟢 Database tables are unchanged."
+    exit 1;
 else
     if [ $(git status --porcelain ./lib/models/database/database.dart | wc -l) -eq "0" ]; then
         echo "  🔴 Database version not updated. Quit"
+        exit 0;
     else
         echo "  🟢 Database version updated."
+        exit 1;
     fi
-    exit 1
 fi
