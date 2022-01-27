b28f04159e061c922c58b169a45c8d0ee2b86025
Javed Hussein <jdhn42@gmail.com>
2022-01-26 23:09:43 -0500

    refactor: update database_checker

--- database_checker.sh
+++ database_checker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ $(git status --porcelain ./lib/models/tables | wc -l) -eq "0" ]; then
+if [ $(git diff @{u}...HEAD ./lib/models/tables| wc -l) -eq "0" ]; then
     echo "  🟢 Database tables are unchanged."
     exit 0;
 else
-    if [ $(git status --porcelain ./lib/models/database/database.dart | wc -l) -eq "0" ]; then
+    if [ $(git diff @{u}...HEAD ./lib/models/database/database.dart | wc -l) -eq "0" ]; then
         echo "  🔴 Database version not updated. Quit"
         exit 1;
     else
