bd8933b4e0e00136151e25a63f5931f8b3b9f037
Javed Hussein <jdhn42@gmail.com>
2022-02-03 15:54:39 -0500

    refactor: remove random count test

--- test/utils/num_to_string_parsers_test.dart
+++ test/utils/num_to_string_parsers_test.dart
@@ -1,10 +1,34 @@
-import 'dart:math';
-
 import 'package:ardrive/utils/num_to_string_parsers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('num to string parser tests', () {
+    test(
+        'fileAndFolderCountsToString returns the correct results '
+        'for 0 files and 0 folders', () {
+      expect(
+        fileAndFolderCountsToString(folderCount: 0, fileCount: 0),
+        equals('0 folders, 0 files'),
+      );
+    });
+
+    test(
+        'fileAndFolderCountsToString returns the correct results '
+        'for 0 files and 1 folder', () {
+      expect(
+        fileAndFolderCountsToString(folderCount: 1, fileCount: 0),
+        equals('1 folder, 0 files'),
+      );
+    });
+    test(
+        'fileAndFolderCountsToString returns the correct results '
+        'for 1 file and 0 folders', () {
+      expect(
+        fileAndFolderCountsToString(folderCount: 0, fileCount: 1),
+        equals('0 folders, 1 file'),
+      );
+    });
+
     test(
         'fileAndFolderCountsToString returns the correct results '
         'for 1 file and 1 folder', () {
@@ -15,22 +39,27 @@ void main() {
     });
     test(
         'fileAndFolderCountsToString returns the correct results '
-        'for 0 files and 0 folders', () {
+        'for 2 files and 1 folder', () {
       expect(
-        fileAndFolderCountsToString(folderCount: 0, fileCount: 0),
-        equals('0 folders, 0 files'),
+        fileAndFolderCountsToString(folderCount: 1, fileCount: 2),
+        equals('1 folder, 2 files'),
+      );
+    });
+
+    test(
+        'fileAndFolderCountsToString returns the correct results '
+        'for 1 file and 2 folders', () {
+      expect(
+        fileAndFolderCountsToString(folderCount: 2, fileCount: 1),
+        equals('2 folders, 1 file'),
       );
     });
     test(
         'fileAndFolderCountsToString returns the correct results '
-        'for multiple files and multiple folders', () {
-      //Generate random non zero counts
-      final fileCount = Random().nextInt(9000) + 1;
-      final folderCount = Random().nextInt(9000) + 1;
+        'for 2 files and 2 folders', () {
       expect(
-        fileAndFolderCountsToString(
-            folderCount: folderCount, fileCount: fileCount),
-        equals('$folderCount folders, $fileCount files'),
+        fileAndFolderCountsToString(folderCount: 2, fileCount: 2),
+        equals('2 folders, 2 files'),
       );
     });
   });
