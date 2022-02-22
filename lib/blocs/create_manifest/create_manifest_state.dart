f3e8a71b2240cb75f18711adcc687dd12a6120ad
Derek Sonnenberg <derek@ardrive.io>
2022-02-22 14:09:27 -0600

    docs(manifests): Add docblocks for states PE-815

--- lib/blocs/create_manifest/create_manifest_state.dart
+++ lib/blocs/create_manifest/create_manifest_state.dart
@@ -6,25 +6,25 @@ abstract class CreateManifestState extends Equatable {
   List<Object> get props => [];
 }
 
+/// Initial state where user begins by selecting a name for the manifest
 class CreateManifestInitial extends CreateManifestState {}
 
+/// Name has been selected, user is now selecting which folder to create a manifest of
 class CreateManifestFolderLoadSuccess extends CreateManifestState {
   final bool viewingRootFolder;
   final FolderWithContents viewingFolder;
 
-  /// The id of the folder/file entry being moved.
-  final String movingEntryId;
-
   CreateManifestFolderLoadSuccess({
     required this.viewingRootFolder,
     required this.viewingFolder,
-    required this.movingEntryId,
   });
 
   @override
-  List<Object> get props => [viewingRootFolder, viewingFolder, movingEntryId];
+  List<Object> get props => [viewingRootFolder, viewingFolder];
 }
 
+/// User selected a folder, but there is an existing non-manifest FILE or FOLDER entity
+/// with a conflicting name. User must re-name the manifest or abort the action
 class CreateManifestNameConflict extends CreateManifestState {
   final String name;
   final FolderEntry parentFolder;
@@ -33,6 +33,8 @@ class CreateManifestNameConflict extends CreateManifestState {
   List<Object> get props => [name, parentFolder];
 }
 
+/// User selected a folder, but there is an existing manifest with a conflicting name
+/// Prompt the user to confirm that this is a revision upload or abort the action
 class CreateManifestRevisionConfirm extends CreateManifestState {
   final FileID id;
   final FolderEntry parentFolder;
@@ -41,10 +43,17 @@ class CreateManifestRevisionConfirm extends CreateManifestState {
   List<Object> get props => [id, parentFolder];
 }
 
+/// Name conflicts have been resolved and uploading the manifest transaction has begun
 class CreateManifestUploadInProgress extends CreateManifestState {}
 
+/// Private drive has been detected, create manifest must be aborted
 class CreateManifestPrivacyMismatch extends CreateManifestState {}
 
+/// Provided wallet does not match the expected wallet, create manifest must be aborted
 class CreateManifestWalletMismatch extends CreateManifestState {}
 
+/// Manifest transaction upload has failed
+class CreateManifestFailure extends CreateManifestState {}
+
+/// Manifest transaction has been successfully uploaded
 class CreateManifestSuccess extends CreateManifestState {}
