28eb1263540b2e6670bac5c232c1c690417f116b
Javed Hussein <jdhn42@gmail.com>
2022-03-07 14:44:15 +0530

    fix(pst): use verto for pst with smartweave as fallback

--- lib/services/pst/pst.dart
+++ lib/services/pst/pst.dart
@@ -1,24 +1,34 @@
-import 'implementations/pst_web.dart'
-    if (dart.library.io) 'implementations/pst_stub.dart' as implementation;
+import 'implementations/pst_smartweave.dart' as smartweave;
+import 'implementations/pst_verto.dart' as verto;
 
 export 'enums.dart';
 
 class PstService {
   /// Returns the fee percentage of the app PST as a decimal percentage.
-  Future<double> getPstFeePercentage() => implementation.getPstFeePercentage();
+  Future<double> getPstFeePercentage() {
+    return verto.getPstFeePercentage().onError((error, stackTrace) {
+      print('Cannot fetch pst fee from verto, falling back on smartweave');
+      return smartweave.getPstFeePercentage();
+    });
+  }
 
   /// Returns a randomly selected address for the holder of the app PST weighted by their holdings.
-  Future<String> getWeightedPstHolder() =>
-      implementation.getWeightedPstHolder();
+  Future<String> getWeightedPstHolder() {
+    return verto.getWeightedPstHolder().onError((error, stackTrace) {
+      print('Cannot fetch pst fee from verto, falling back on smartweave');
+      return smartweave.getWeightedPstHolder();
+    });
+  }
 
   Future<BigInt> getPSTFee(BigInt uploadCost) async {
-    return await implementation
-        .getPstFeePercentage()
+    return await getPstFeePercentage()
         .then((feePercentage) =>
             // Workaround [BigInt] percentage division problems
             // by first multiplying by the percentage * 100 and then dividing by 100.
             uploadCost * BigInt.from(feePercentage * 100) ~/ BigInt.from(100))
-        .catchError((_) => BigInt.zero,
-            test: (err) => err is UnimplementedError);
+        .catchError(
+          (_) => BigInt.zero,
+          test: (err) => err is UnimplementedError,
+        );
   }
 }
