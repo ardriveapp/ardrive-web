8cd0aa29f68751a16b503cd904e1b8309a5f26f5
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-03-07 18:01:36 -0300

    feat: Remove xfilesToUploadPlan from UploadCubit

--- lib/blocs/upload/upload_cubit.dart
+++ lib/blocs/upload/upload_cubit.dart
@@ -2,25 +2,19 @@ import 'dart:async';
 import 'dart:math' as math;
 
 import 'package:ardrive/blocs/upload/cost_estimate.dart';
-import 'package:ardrive/blocs/upload/data_item_upload_handle.dart';
 import 'package:ardrive/blocs/upload/upload_plan.dart';
-import 'package:ardrive/entities/entities.dart';
 import 'package:ardrive/models/models.dart';
 import 'package:ardrive/services/services.dart';
 import 'package:ardrive/utils/upload_plan_utils.dart';
-import 'package:arweave/arweave.dart';
 import 'package:bloc/bloc.dart';
-import 'package:cryptography/cryptography.dart';
 import 'package:equatable/equatable.dart';
 import 'package:file_selector/file_selector.dart';
 import 'package:meta/meta.dart';
-import 'package:mime/mime.dart';
 import 'package:pedantic/pedantic.dart';
 import 'package:uuid/uuid.dart';
 
 import '../blocs.dart';
 import 'enums/conflicting_files_actions.dart';
-import 'file_upload_handle.dart';
 
 part 'upload_state.dart';
 
@@ -221,68 +215,6 @@ class UploadCubit extends Cubit<UploadState> {
     unawaited(_profileCubit.refreshBalance());
 
     emit(UploadComplete());
-  }
-
-  Future<UploadPlan> xfilesToUploadPlan({
-    required List<XFile> files,
-    required SecretKey cipherKey,
-    required Wallet wallet,
-  }) async {
-    final _dataItemUploadHandles = <String, DataItemUploadHandle>{};
-    final _v2FileUploadHandles = <String, FileUploadHandle>{};
-    for (var file in files) {
-      final fileName = file.name;
-      final filePath = '${_targetFolder.path}/$fileName';
-      final fileSize = await file.length();
-      final fileEntity = FileEntity(
-        driveId: _targetDrive.id,
-        name: fileName,
-        size: fileSize,
-        lastModifiedDate: await file.lastModified(),
-        parentFolderId: _targetFolder.id,
-        dataContentType: lookupMimeType(fileName) ?? 'application/octet-stream',
-      );
-
-      // If this file conflicts with one that already exists in the target folder reuse the id of the conflicting file.
-      fileEntity.id = conflictingFiles[fileName] ?? _uuid.v4();
-
-      final private = _targetDrive.isPrivate;
-      final driveKey = private
-          ? await _driveDao.getDriveKey(_targetDrive.id, cipherKey)
-          : null;
-      final fileKey =
-          private ? await deriveFileKey(driveKey!, fileEntity.id!) : null;
-
-      final revisionAction = !conflictingFiles.containsKey(file.name)
-          ? RevisionAction.create
-          : RevisionAction.uploadNewVersion;
-
-      if (fileSize < bundleSizeLimit) {
-        _dataItemUploadHandles[fileEntity.id!] = DataItemUploadHandle(
-          entity: fileEntity,
-          path: filePath,
-          file: file,
-          driveKey: driveKey,
-          fileKey: fileKey,
-          arweave: _arweave,
-          wallet: wallet,
-          revisionAction: revisionAction,
-        );
-      } else {
-        _v2FileUploadHandles[fileEntity.id!] = FileUploadHandle(
-          entity: fileEntity,
-          path: filePath,
-          file: file,
-          driveKey: driveKey,
-          fileKey: fileKey,
-          revisionAction: revisionAction,
-        );
-      }
-    }
-    return UploadPlan.create(
-      v2FileUploadHandles: _v2FileUploadHandles,
-      dataItemUploadHandles: _dataItemUploadHandles,
-    );
   }
 
   void _removeConflictingFiles() {
