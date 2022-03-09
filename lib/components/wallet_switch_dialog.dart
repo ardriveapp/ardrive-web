07273596c13008a5b19e41774f433afdc02ea970
Mati <matibat@gmail.com>
2022-03-08 21:19:44 -0300

    feat(interationalization): WIP-5 PE-1078

--- lib/components/wallet_switch_dialog.dart
+++ lib/components/wallet_switch_dialog.dart
@@ -1,9 +1,9 @@
-
 import 'package:ardrive/blocs/blocs.dart';
 import 'package:ardrive/components/app_dialog.dart';
 import 'package:ardrive/utils/html/html_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class WalletSwitchDialog extends StatelessWidget {
   final bool fromAuthPage;
@@ -13,13 +13,11 @@ class WalletSwitchDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) => AppDialog(
         dismissable: false,
-        title: 'Wallet Switch',
+        title: AppLocalizations.of(context)!.walletSwitch,
         content: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text('Wallet change detected. Please log out and log in again')
-          ],
+          children: [Text(AppLocalizations.of(context)!.walletChangeDetected)],
         ),
         actions: [
           TextButton(
@@ -31,7 +29,7 @@ class WalletSwitchDialog extends StatelessWidget {
                 context.read<ProfileAddCubit>().promptForWallet();
               }
             },
-            child: Text('Logout'),
+            child: Text(AppLocalizations.of(context)!.logOut),
           )
         ],
       );
