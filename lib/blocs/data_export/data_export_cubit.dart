83e81a813190e515df0744dff9524cc1096be6fe
Mati <matibat@gmail.com>
2022-03-09 09:23:27 -0300

    feat(interationalization): WIP-7 PE-1078

--- lib/blocs/data_export/data_export_cubit.dart
+++ lib/blocs/data_export/data_export_cubit.dart
@@ -65,6 +65,8 @@ class DataExportCubit extends Cubit<DataExportState> {
     emit(DataExportSuccess(
       file: XFile.fromData(
         dataBytes,
+        // FIXME: context is not available here. Internationalization cannot be applied
+        // name: AppLocalizations.of(context)!.exportFromCSV(driveId, DateTime.now().toString()),
         name: 'Export from $driveId ${DateTime.now().toString()}.csv',
         mimeType: 'text/csv',
         length: dataBytes.lengthInBytes,
