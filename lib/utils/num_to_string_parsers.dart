78ce734baa6d29bf9f94b09f1a77e3d9b6ddb322
Mati <matibat@gmail.com>
2022-03-11 17:48:30 -0300

    feat(internationalization): folderAndFile count translations are consolidated PE-1078

--- lib/utils/num_to_string_parsers.dart
+++ lib/utils/num_to_string_parsers.dart
@@ -5,32 +5,8 @@ String fileAndFolderCountsToString({
   required int fileCount,
   required AppLocalizations localizations,
 }) {
-  if (folderCount == 1 && fileCount == 1) {
-    // 1-1
-    return localizations.folderAndFile(folderCount, fileCount);
-  } else if (folderCount == 1 && fileCount > 1) {
-    // 1-N
-    return localizations.folderAndFiles(folderCount, fileCount);
-  } else if (folderCount > 1 && fileCount == 1) {
-    // N-1
-    return localizations.foldersAndFile(folderCount, fileCount);
-  } else if (folderCount > 1 && fileCount > 1) {
-    // N-N
-    return localizations.foldersAndFiles(folderCount, fileCount);
-  } else if (folderCount == 0 && fileCount == 1) {
-    // 0-1
-    return localizations.zeroFoldersAndFile(folderCount, fileCount);
-  } else if (folderCount == 1 && fileCount == 0) {
-    // 1-0
-    return localizations.folderAndZeroFiles(folderCount, fileCount);
-  } else if (folderCount == 0 && fileCount == 0) {
-    // 0-0
-    return localizations.zeroFoldersAndZeroFiles(folderCount, fileCount);
-  } else if (folderCount > 1 && fileCount == 0) {
-    // N-0
-    return localizations.foldersAndZeroFiles(folderCount, fileCount);
-  } else {
-    // 0-N
-    return localizations.zeroFoldersAndFiles(folderCount, fileCount);
-  }
+  final folderCountString = localizations.folderCount(folderCount);
+  final fileCountString = localizations.fileCount(fileCount);
+  return localizations.folderAndFileCountComposite(
+      folderCountString, fileCountString);
 }
