672fac29f12c59e0b9ae55a77b766e571cdb1145
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-05-26 09:38:08 -0300

    Add animation

--- lib/components/file_download_dialog.dart
+++ lib/components/file_download_dialog.dart
@@ -39,7 +39,9 @@ showDownloadOverlay({
           driveDao: context.read<DriveDao>(),
           arweave: context.read<ArweaveService>(),
         ),
-        child: Material(child: FileDownloadFloating()),
+        child: Material(
+            borderRadius: BorderRadius.circular(15),
+            child: FileDownloadFloating()),
       ),
     ),
   );
@@ -86,82 +88,150 @@ Future<void> promptToDownloadSharedFile({
     );
 
 class FileDownloadFloating extends StatelessWidget {
-  const FileDownloadFloating({Key? key}) : super(key: key);
-
+  FileDownloadFloating({Key? key}) : super(key: key);
+  final controller = DownloadBarController();
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<FileDownloadCubit, FileDownloadState>(
       listener: (context, state) async {
+        if (state is FileDownloadInProgress) {
+          controller.startDownload();
+        }
         if (state is FileDownloadSuccess) {
-          entry.remove();
           final savePath = await getSavePath();
-          if (savePath != null) {
-            unawaited(state.file.saveTo(savePath));
-          }
+          controller.endDownload();
+          Future.delayed(Duration(milliseconds: 2000)).then((value) {
+            if (savePath != null) {
+              unawaited(state.file.saveTo(savePath));
+            }
+            entry.remove();
+          });
 
           Navigator.pop(context);
         }
       },
       builder: (context, state) {
-        if (state is FileDownloadInProgress) {
-          return SizedBox(
-            width: 300,
-            height: 80,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.circular(10)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ProgressBar(
-                      percentage: downloadStream,
-                      darkMode: true,
-                    ),
-                    StreamBuilder<DownloadProgress>(
-                      stream: downloadStream,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final speed =
-                              snapshot.data!.speed!.toStringAsFixed(2);
-                          String timeFormatter(int time) {
-                            Duration duration = Duration(seconds: time.round());
-
-                            return [
-                              if (duration.inMinutes > 60) duration.inHours,
-                              if (duration.inSeconds > 60) duration.inMinutes,
-                              duration.inSeconds
-                            ]
-                                .map((seg) => seg
-                                    .remainder(60)
-                                    .toString()
-                                    .padLeft(2, '0'))
-                                .join(':');
-                          }
-
-                          final remainingTime = snapshot.data!.remainingTime;
-
-                          return Text(
-                            'Download speed: $speed MB/s Remaining time: ${timeFormatter(remainingTime!)}s',
-                            style: TextStyle(color: Colors.white),
-                          );
-                        }
-                        return Container();
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        }
-        return const SizedBox(
-          height: 300,
-          width: 300,
-        );
+        return DownloadBar(controller: controller);
       },
+    );
+  }
+}
+
+enum DownloadStatus { inProgress, done }
+
+class DownloadBarController {
+  final _downloadController = StreamController<DownloadStatus>();
+  startDownload() => _downloadController.sink.add(DownloadStatus.inProgress);
+  endDownload() => _downloadController.sink.add(DownloadStatus.done);
+}
+
+class DownloadBar extends StatefulWidget {
+  const DownloadBar({Key? key, required this.controller}) : super(key: key);
+
+  final DownloadBarController controller;
+
+  @override
+  State<DownloadBar> createState() => _DownloadBarState();
+}
+
+class _DownloadBarState extends State<DownloadBar> {
+  double width = 0;
+  double height = 100;
+  bool downloadCompleted = false;
+  bool downloadEndPhase = false;
+  Color background = Colors.black87;
+
+  @override
+  void initState() {
+    widget.controller._downloadController.stream.listen((event) {
+      if (event == DownloadStatus.inProgress) {
+        Future.delayed(Duration(milliseconds: 100))
+            .then((value) => setState(() {
+                  width = 300;
+                }));
+      } else {
+        setState(() {
+          downloadCompleted = true;
+          background = Colors.green;
+        });
+        Future.delayed(Duration(milliseconds: 1000))
+            .then((value) => setState(() {
+                  downloadEndPhase = true;
+                  width = 0;
+                }));
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      width: width,
+      height: height,
+      curve: Curves.decelerate,
+      duration: const Duration(milliseconds: 1000),
+      decoration: BoxDecoration(
+          color: background, borderRadius: BorderRadius.circular(10)),
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: downloadCompleted && !downloadEndPhase
+            ? Center(
+                child: Text(
+                  'Download finished',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              )
+            : downloadEndPhase
+                ? Container()
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ProgressBar(
+                          percentage: downloadStream,
+                          darkMode: true,
+                        ),
+                        StreamBuilder<DownloadProgress>(
+                          stream: downloadStream,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              final speed =
+                                  snapshot.data!.speed!.toStringAsFixed(2);
+                              String timeFormatter(int time) {
+                                Duration duration =
+                                    Duration(seconds: time.round());
+
+                                return [
+                                  if (duration.inMinutes > 60) duration.inHours,
+                                  if (duration.inSeconds > 60)
+                                    duration.inMinutes,
+                                  duration.inSeconds
+                                ]
+                                    .map((seg) => seg
+                                        .remainder(60)
+                                        .toString()
+                                        .padLeft(2, '0'))
+                                    .join(':');
+                              }
+
+                              final remainingTime =
+                                  snapshot.data!.remainingTime;
+
+                              return Text(
+                                'Download speed: $speed MB/s\nRemaining time: ${timeFormatter(remainingTime!)}s',
+                                style: TextStyle(color: Colors.white),
+                              );
+                            }
+                            return Container();
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+      ),
     );
   }
 }
