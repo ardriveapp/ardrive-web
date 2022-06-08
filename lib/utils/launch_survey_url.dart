9741c12d1bbcff069013b6d60c150fa8eeb8fc49
Mati <matibat@gmail.com>
2022-06-08 17:53:31 -0300

    chore(feedback modal): moves the url to the resources file PE-1662

--- lib/utils/launch_survey_url.dart
+++ lib/utils/launch_survey_url.dart
@@ -1,7 +1,8 @@
+import 'package:ardrive/misc/resources.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchSurveyURL() async {
-  const url = 'https://ardrive.typeform.com/UserSurvey';
+  const url = R.surveyFeedbackFormUrl;
   if (await canLaunch(url)) {
     await launch(url);
   } else {
