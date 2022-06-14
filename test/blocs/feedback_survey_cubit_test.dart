0a7b908175967701b8f3d11713fb0a7f6a16de5d
Mati <matibat@gmail.com>
2022-06-14 15:44:27 -0300

    test(feedback cubit): updates the tests with the new behavior PE-1664

--- test/blocs/feedback_survey_cubit_test.dart
+++ test/blocs/feedback_survey_cubit_test.dart
@@ -1,28 +1,33 @@
 import 'package:ardrive/blocs/feedback_survey/feedback_survey_cubit.dart';
+import 'package:ardrive/utils/key_value_store.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../test_utils/fakes.dart';
 
 void main() {
   group('FeedbackSurveyCubit', () {
+    final store = KeyValueStore();
     late FeedbackSurveyCubit feedbackCubit;
 
-    setUp(() {
+    setUp(() async {
       registerFallbackValue(SyncStatefake());
       registerFallbackValue(ProfileStatefake());
+
+      Map<String, Object> values = <String, Object>{};
+      SharedPreferences.setMockInitialValues(values);
+      final fakePrefs = await SharedPreferences.getInstance();
+      await store.setup(instance: fakePrefs);
+
       feedbackCubit = FeedbackSurveyCubit(FeedbackSurveyInitialState());
     });
-
-    tearDown(() async {});
 
     blocTest<FeedbackSurveyCubit, FeedbackSurveyState>(
       'open modal',
       build: () => feedbackCubit,
-      act: (bloc) async {
-        bloc.openRemindMe();
-      },
+      act: (bloc) async => await bloc.openRemindMe(),
       expect: () => [
         FeedbackSurveyRemindMe(isOpen: true),
       ],
@@ -31,9 +36,7 @@ void main() {
     blocTest<FeedbackSurveyCubit, FeedbackSurveyState>(
       'leave feedback',
       build: () => feedbackCubit,
-      act: (bloc) async {
-        bloc.leaveFeedback();
-      },
+      act: (bloc) async => await bloc.leaveFeedback(),
       expect: () => [
         FeedbackSurveyDontRemindMe(isOpen: false),
       ],
@@ -42,9 +45,7 @@ void main() {
     blocTest<FeedbackSurveyCubit, FeedbackSurveyState>(
       'dismiss modal',
       build: () => feedbackCubit,
-      act: (bloc) async {
-        bloc.closeRemindMe();
-      },
+      act: (bloc) => bloc.closeRemindMe(),
       expect: () => [
         FeedbackSurveyRemindMe(isOpen: false),
       ],
@@ -53,12 +54,37 @@ void main() {
     blocTest<FeedbackSurveyCubit, FeedbackSurveyState>(
       'no thanks! dont remind me again',
       build: () => feedbackCubit,
-      act: (bloc) async {
-        bloc.closeDontRemindMe();
-      },
+      act: (bloc) async => await bloc.closeDontRemindMe(),
       expect: () => [
         FeedbackSurveyDontRemindMe(isOpen: false),
       ],
+    );
+  });
+
+  group('FeedbackSurveyCubit preferences', () {
+    final store = KeyValueStore();
+    late FeedbackSurveyCubit feedbackCubit;
+
+    setUp(() async {
+      registerFallbackValue(SyncStatefake());
+      registerFallbackValue(ProfileStatefake());
+
+      Map<String, Object> values = <String, Object>{
+        FeedbackSurveyCubit.dontRemindMeAgainKey: true
+      };
+      SharedPreferences.setMockInitialValues(values);
+      final fakePrefs = await SharedPreferences.getInstance();
+      await store.setup(instance: fakePrefs);
+
+      feedbackCubit =
+          FeedbackSurveyCubit(FeedbackSurveyInitialState(), store: store);
+    });
+
+    blocTest<FeedbackSurveyCubit, FeedbackSurveyState>(
+      'won\'t open modal when set',
+      build: () => feedbackCubit,
+      act: (bloc) async => await bloc.openRemindMe(),
+      expect: () => [],
     );
   });
 }
