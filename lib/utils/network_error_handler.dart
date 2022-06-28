917a78849009b68c69b2932a9a2e98fe711954e8
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-06-28 16:16:16 -0300

    refactor(handle 5xx errors) add variable to dont repeat null check PE-1747

--- lib/utils/network_error_handler.dart
+++ lib/utils/network_error_handler.dart
@@ -5,20 +5,20 @@ class NetworkErrorHandler {
   NetworkError handle(Response response) {
     final requestRoute = response.request?.url.path;
     final statusCode = response.statusCode;
+    final reasonPhrase = response.reasonPhrase ?? '';
     if (response.statusCode >= 500) {
       return ServerError(
           statusCode: statusCode,
           requestRoute: requestRoute,
-          reasonPhrase: response.reasonPhrase ?? '');
+          reasonPhrase: reasonPhrase);
     }
     if (response.statusCode == 429) {
       return RateLimitError(
-          requestRoute: requestRoute,
-          reasonPhrase: response.reasonPhrase ?? '');
+          requestRoute: requestRoute, reasonPhrase: reasonPhrase);
     }
     return UnknownNetworkError(
         statusCode: statusCode,
         requestRoute: requestRoute,
-        reasonPhrase: response.reasonPhrase ?? '');
+        reasonPhrase: reasonPhrase);
   }
 }
