ed14fe30b0876e01258dc936c85caf29bca4360f
Mati <matibat@gmail.com>
2022-06-28 18:26:39 -0300

    feat(profile cubit): use a BlockListener instead of calling a cubit from another PE-1774

--- lib/blocs/profile/profile_cubit.dart
+++ lib/blocs/profile/profile_cubit.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:ardrive/blocs/feedback_survey/feedback_survey_cubit.dart';
 import 'package:ardrive/entities/profileTypes.dart';
 import 'package:ardrive/models/models.dart';
 import 'package:ardrive/services/arconnect/arconnect_wallet.dart';
@@ -20,17 +19,14 @@ class ProfileCubit extends Cubit<ProfileState> {
   final ArweaveService _arweave;
   final ProfileDao _profileDao;
   final Database _db;
-  final FeedbackSurveyCubit _feedbackSurveyCubit;
 
   ProfileCubit({
     required ArweaveService arweave,
     required ProfileDao profileDao,
     required Database db,
-    required FeedbackSurveyCubit feedbackSurveyCubit,
   })  : _arweave = arweave,
         _profileDao = profileDao,
         _db = db,
-        _feedbackSurveyCubit = feedbackSurveyCubit,
         super(ProfileCheckingAvailability()) {
     promptToAuthenticate();
   }
@@ -165,8 +161,6 @@ class ProfileCubit extends Cubit<ProfileState> {
   Future<void> logoutProfile() async {
     final profile = await _profileDao.defaultProfile().getSingleOrNull();
     final arconnect = ArConnectService();
-
-    _feedbackSurveyCubit.reset();
 
     if (profile != null && profile.profileType == ProfileType.ArConnect.index) {
       try {
