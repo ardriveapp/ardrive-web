a175eaafeba08f538eb71641fbf3647e6045a78d
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-06-28 16:13:16 -0300

    refactor(handle 5xx errors) improve test descriptions on http retry tests PE-1747

--- test/utils/http_retry_test.dart
+++ test/utils/http_retry_test.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart';
 void main() {
   HttpRetry sut = HttpRetry(NetworkErrorHandler());
 
-  group('Testing HttpRetry', () {
+  group('Testing HttpRetry class', () {
     const timeoutForWaitRetries = Timeout(Duration(minutes: 1));
 
     final success = Response('body', 200);
@@ -17,30 +17,43 @@ void main() {
     final serverError500 = Response('body', 500);
     final serverError504 = Response('body', 504);
 
-    test('should return the response', () async {
+    test('call method should return the response when dont have any error',
+        () async {
       final response = await sut.call(() async => success);
       expect(response, success);
     }, timeout: timeoutForWaitRetries);
 
-    test('should throw RateLimitError', () async {
+    test(
+        'call method should throw RateLimitError when response has status code 429',
+        () async {
       expect(() async => await sut.call(() async => rateLimit),
           throwsA(const TypeMatcher<RateLimitError>()));
     }, timeout: timeoutForWaitRetries);
 
-    test('should throw UnknownNetworkError', () async {
+    test(
+        'call method should throw UnknownNetworkError when response has status different from 5xx or 429',
+        () async {
       expect(() async => await sut.call(() async => unknownError),
           throwsA(const TypeMatcher<UnknownNetworkError>()));
     }, timeout: timeoutForWaitRetries);
 
-    test('should throw ServerError for status code 502', () async {
+    test(
+        'call method should throw ServerError for response with status code 502',
+        () async {
       expect(() async => await sut.call(() async => serverError502),
           throwsA(const TypeMatcher<ServerError>()));
     }, timeout: timeoutForWaitRetries);
-    test('should throw ServerError for status code 504', () async {
+
+    test(
+        'call method should throw ServerError for response with status code 504',
+        () async {
       expect(() async => await sut.call(() async => serverError504),
           throwsA(const TypeMatcher<ServerError>()));
     }, timeout: timeoutForWaitRetries);
-    test('should throw ServerError for status code 500', () async {
+
+    test(
+        'call method should throw ServerError for response with status code 500',
+        () async {
       expect(() async => await sut.call(() async => serverError500),
           throwsA(const TypeMatcher<ServerError>()));
     }, timeout: timeoutForWaitRetries);
