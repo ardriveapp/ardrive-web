07852311e20f33f624c9d49273828660ee73e39b
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-06-30 17:20:10 -0300

    refactor(handle 5xx errors) fix a wrong 2xx status code verification PE-1747.

--- lib/services/arweave/error/gateway_response_handler.dart
+++ lib/services/arweave/error/gateway_response_handler.dart
@@ -2,13 +2,15 @@ import 'package:ardrive/services/arweave/error/gateway_error.dart';
 import 'package:ardrive/utils/response_handler.dart';
 import 'package:http/http.dart';
 
-/// Throws a `GatewayError` exception for `statusCode` different from 200
+/// Throws a `GatewayError` exception for `statusCode` different from 2xx
 ///
 class GatewayResponseHandler implements ResponseHandler {
   @override
   void handle(Response response) {
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      throw GatewayError.fromResponse(response);
+      return;
     }
+
+    throw GatewayError.fromResponse(response);
   }
 }
