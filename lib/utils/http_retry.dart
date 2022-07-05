e552b356f756ea2910580b2f87a32238b0330007
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-07-05 11:21:59 -0300

    refactor(handle 5xx errors) improve log at HttpRetry PE-1747.

--- lib/utils/http_retry.dart
+++ lib/utils/http_retry.dart
@@ -5,10 +5,11 @@ import 'package:http/http.dart';
 import 'package:retry/retry.dart';
 
 class HttpRetryOptions {
-  HttpRetryOptions({this.retryIf, this.onRetry});
+  HttpRetryOptions({this.retryIf, this.onRetry, this.maxAttempts = 8});
 
   final FutureOr<bool> Function(Exception)? retryIf;
   final FutureOr<void> Function(Exception)? onRetry;
+  final int maxAttempts;
 }
 
 class HttpRetry {
@@ -27,11 +28,13 @@ class HttpRetry {
           responseHandler.handle(response);
 
           if (retryAttempts > 0) {
-            print('Succesfully get the response after $retryAttempts');
+            print(
+                'Succesfully get the response after $retryAttempts attempt(s)');
           }
 
           return response;
         },
+        maxAttempts: httpRetryOptions.maxAttempts,
         randomizationFactor: 0,
         onRetry: (e) {
           ++retryAttempts;
