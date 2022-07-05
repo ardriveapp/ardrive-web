fa118726126587fc8542cdecae3349b642346f21
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-07-05 09:27:04 -0300

    refactor(handle 5xx errors) pass by dependency injection the options with the callbacks onRetry and retryIf in order to test those functions PE-1747.

--- lib/utils/http_retry.dart
+++ lib/utils/http_retry.dart
@@ -4,21 +4,39 @@ import 'package:ardrive/utils/response_handler.dart';
 import 'package:http/http.dart';
 import 'package:retry/retry.dart';
 
+class HttpRetryOptions {
+  HttpRetryOptions({this.retryIf, this.onRetry});
+
+  final FutureOr<bool> Function(Exception)? retryIf;
+  final FutureOr<void> Function(Exception)? onRetry;
+}
+
 class HttpRetry {
-  HttpRetry(this.responseHandler);
+  HttpRetry(this.responseHandler, this.httpRetryOptions);
 
   final ResponseHandler responseHandler;
+  final HttpRetryOptions httpRetryOptions;
 
-  Future<Response> processRequest(Future<Response> Function() request,
-      {FutureOr<bool> Function(Exception)? retryIf,
-      FutureOr<void> Function(Exception)? onRetry}) {
-    return retry(() async {
-      final response = await request();
+  Future<Response> processRequest(Future<Response> Function() request) {
+    int retryTimes = 0;
+    return retry(
+        () async {
+          final response = await request();
 
-      /// Handle errors if have
-      responseHandler.handle(response);
+          /// Handle errors if have
+          responseHandler.handle(response);
 
-      return response;
-    }, onRetry: onRetry, retryIf: retryIf);
+          if (retryTimes > 0) {
+            print('Succesfully get the response after $retryTimes');
+          }
+
+          return response;
+        },
+        randomizationFactor: 0,
+        onRetry: (e) {
+          ++retryTimes;
+          httpRetryOptions.onRetry?.call(e);
+        },
+        retryIf: httpRetryOptions.retryIf);
   }
 }
--- test/utils/http_retry_test.dart
+++ test/utils/http_retry_test.dart
@@ -6,11 +6,16 @@ import 'package:mocktail/mocktail.dart';
 
 class MockResponseHandler extends Mock implements ResponseHandler {}
 
+class MockRetryHttpOptions extends Mock implements HttpRetryOptions {}
+
 void main() {
   const timeoutForWaitRetries = Timeout(Duration(minutes: 1));
+  const retryMaxAttempts = 8;
 
   final mockResponseHandler = MockResponseHandler();
-  HttpRetry sut = HttpRetry(mockResponseHandler);
+  final mockRetryHttpOptions = MockRetryHttpOptions();
+
+  HttpRetry sut = HttpRetry(mockResponseHandler, mockRetryHttpOptions);
 
   group('Testing HttpRetry class', () {
     final tResponse = Response('body', 200);
@@ -20,16 +25,26 @@ void main() {
     });
 
     test('Should return the response when dont have any errors', () async {
-      when(() => mockResponseHandler.handle(any())).thenAnswer((i) => null);
+      when(() => mockResponseHandler.handle(any())).thenAnswer((i) => Null);
+
       final response = await sut.processRequest(() async => tResponse);
       expect(response, tResponse);
     });
 
     test('Should retry and throw when ResponseHandler throws', () async {
       when(() => mockResponseHandler.handle(any())).thenThrow(Exception());
-
-      expect(() => sut.processRequest(() async => tResponse),
+      when(() => mockRetryHttpOptions.onRetry!(any()))
+          .thenAnswer((i) => print('calling the callback...'));
+      await expectLater(() => sut.processRequest(() async => tResponse),
           throwsA(const TypeMatcher<Exception>()));
+
+      /// Verifies if has retried the expected times
+      /// On the last time trying, it will return the response in case of success
+      /// or throw the exception in case of failure, so the `onRetry` function
+      /// won't be called in the last time.
+      verify(
+        () => mockRetryHttpOptions.onRetry?.call(any()),
+      ).called(retryMaxAttempts - 1);
     }, timeout: timeoutForWaitRetries);
   });
 }
