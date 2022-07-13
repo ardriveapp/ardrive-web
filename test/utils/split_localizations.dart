caf60fa95d1f5b75fb4e825c081b2cfa5edeef79
Mati <matibat@gmail.com>
2022-07-13 18:02:06 -0300

    test(text with multiple styles): implement a test for the separator PE-1125

--- test/utils/split_localizations.dart
+++ test/utils/split_localizations.dart
@@ -1,37 +1,53 @@
 import 'package:ardrive/utils/split_localizations.dart';
-import 'package:ardrive/utils/text_partitions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('splitTranslationsWithMultipleStyles method', () {
-    final WidgetFactory defaultFactory = (String text) => Text(text);
-    final WidgetFactory anchorFactory = (String text) => TextButton(
+    Widget defaultFactory(String text) => Text(text);
+    Widget anchorFactory(String text) => TextButton(
           child: Text(text),
           onPressed: () => print('PRESS'),
         );
 
-    final partitionsJA =
-        TextPartitions(wholeText: '利用規約と ArDrive の プライバシーポリシーに同意します');
-    final partitionsEN = TextPartitions(
-        wholeText:
-            'I agree to the ArDrive terms of service and privacy policy');
-
     test('returns the expected type of widgets in order', () {
       final widgetsJA = splitTranslationsWithMultipleStyles(
-          '利用規約と ArDrive の プライバシーポリシーに同意します',
-          defaultFactory,
-          {'ArDrive の プライバシーポリシー': anchorFactory});
+        originalText: '利用規約と ArDrive の プライバシーポリシーに同意します',
+        defaultMapper: defaultFactory,
+        parts: {'ArDrive の プライバシーポリシー': anchorFactory},
+      );
       expect(widgetsJA[0] is Text, true);
       expect(widgetsJA[1] is TextButton, true);
       expect(widgetsJA[2] is Text, true);
+      expect(widgetsJA.length, 3);
 
       final widgetsEN = splitTranslationsWithMultipleStyles(
-          'I agree to the ArDrive terms of service and privacy policy',
-          defaultFactory,
-          {'ArDrive terms of service and privacy policy': anchorFactory});
+        originalText:
+            'I agree to the ArDrive terms of service and privacy policy',
+        defaultMapper: defaultFactory,
+        parts: {
+          'ArDrive terms of service and privacy policy': anchorFactory,
+        },
+      );
       expect(widgetsEN[0] is Text, true);
       expect(widgetsEN[1] is TextButton, true);
+      expect(widgetsEN.length, 2);
+    });
+
+    test('puts separators between each part if specified', () {
+      final widgetsEN = splitTranslationsWithMultipleStyles<Widget>(
+        originalText:
+            'I agree to the ArDrive terms of service and privacy policy',
+        defaultMapper: defaultFactory,
+        parts: {
+          'ArDrive terms of service and privacy policy': anchorFactory,
+        },
+        separator: const SizedBox(height: 32, width: 32),
+      );
+      expect(widgetsEN[0] is Text, true);
+      expect(widgetsEN[1] is SizedBox, true);
+      expect(widgetsEN[2] is TextButton, true);
+      expect(widgetsEN.length, 3);
     });
   });
 }
