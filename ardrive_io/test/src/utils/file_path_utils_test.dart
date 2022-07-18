e1d5be5d1c93ade1810dfe8df1d816076200c5aa
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-07-18 17:08:44 -0300

    feat(ardrive_io): validate incorrect path at getExtensionFromPath and add tests PE-1863

---
+++ ardrive_io/test/src/utils/file_path_utils_test.dart
@@ -0,0 +1,19 @@
+import 'package:ardrive_io/src/io_exception.dart';
+import 'package:ardrive_io/src/utils/file_path_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('test method getExtensionFromPath', () {
+    test('should return correct extension', () {
+      expect(getExtensionFromPath('file.txt'), 'txt');
+      expect(getExtensionFromPath('file.jpg'), 'jpg');
+      expect(getExtensionFromPath('some.file.jpg'), 'jpg');
+      expect(getExtensionFromPath('some_directory/some.file.jpg'), 'jpg');
+    });
+
+    test('should throw an EntityPathException when got a empty path', () {
+      expect(() => getExtensionFromPath(''),
+          throwsA(const TypeMatcher<EntityPathException>()));
+    });
+  });
+}
