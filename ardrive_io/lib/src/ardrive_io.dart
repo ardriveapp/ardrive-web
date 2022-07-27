bc656a2dc3bd3befba9f9e1db4e8c965f59dc597
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-07-27 13:53:58 -0300

    feat(ardrive_io iOS) rename andriod file saver class and use for both iOS and Android. Add some comments PE-1868

--- ardrive_io/lib/src/ardrive_io.dart
+++ ardrive_io/lib/src/ardrive_io.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:ardrive_io/ardrive_io.dart';
 import 'package:flutter/foundation.dart';
 
 import 'io_exception.dart';
+import 'io_file.dart';
+import 'io_folder.dart';
 import 'mobile/mobile_io.dart';
 
+/// API for I/O operations
+///
+/// Opens the platform specific file picker to pick files and folders, and save files using
+/// the `IOFile` and `IOFolder` APIs.
 abstract class ArDriveIO {
   Future<IOFile> pickFile({List<String>? allowedExtensions});
   Future<List<IOFile>> pickFiles({List<String>? allowedExtensions});
