893e7889f11009bc99c594112055df8b94ddb68e
Mati <matibat@gmail.com>
2022-08-09 17:01:28 -0300

    test(pst utils): adds a pending test PE-1918

---
+++ test/pst/utils_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('weightedRandom', () {
+    test('returns a random address within the input', () {});
+    test('returns null if the address could not be determined', () {});
+  }, skip: 'this method uses a random number - FIXME: make me testable');
+}
