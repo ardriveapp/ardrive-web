bbe09052bea2f03b23cba701ed439d0e1c732928
Javed Hussein <jdhn42@gmail.com>
2022-08-11 09:49:43 -0400

    test: add test to check invalid data in allfileentities PE-1719

--- test/services/arweave/arweave_service_test.dart
+++ test/services/arweave/arweave_service_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:ardrive/services/services.dart';
 import 'package:arweave/arweave.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -16,7 +18,14 @@ void main(List<String> args) {
       const knownRevisionCount = 9;
       final fileEntities = await arweave.getAllFileEntitiesWithId(knownFileId);
       expect(fileEntities?.length, equals(knownRevisionCount));
-      expect(true, isTrue);
+    });
+    test('AllFileEntitiesWithId returns null for invalid Id', () async {
+      final invalidFileId = Random().nextInt(1000).toString();
+
+      final fileEntities = await arweave.getAllFileEntitiesWithId(
+        invalidFileId,
+      );
+      expect(fileEntities, equals(null));
     });
   });
 }
