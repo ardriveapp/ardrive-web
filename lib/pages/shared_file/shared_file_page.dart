a7bd6a00fe567a4e4219f1afe257654bcbfa5398
Javed Hussein <jdhn42@gmail.com>
2022-08-25 15:18:47 -0400

    fix: fix detail panel height PE-2164

--- lib/pages/shared_file/shared_file_page.dart
+++ lib/pages/shared_file/shared_file_page.dart
@@ -23,107 +23,113 @@ class SharedFilePage extends StatelessWidget {
     return Material(
       child: BlocBuilder<SharedFileCubit, SharedFileState>(
         builder: (context, state) {
-          Widget shareCard() => Card(
-                elevation: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Center(
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        maxWidth: kMediumDialogWidth,
-                        minWidth: kMediumDialogWidth,
-                        minHeight: 256,
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            Resources
-                                .images.brand.logoHorizontalNoSubtitleLight,
-                            height: 96,
-                            fit: BoxFit.contain,
+          Widget shareCard() {
+            return Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Center(
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: kMediumDialogWidth,
+                      minWidth: kMediumDialogWidth,
+                      minHeight: 256,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          Resources.images.brand.logoHorizontalNoSubtitleLight,
+                          height: 96,
+                          fit: BoxFit.contain,
+                        ),
+                        const SizedBox(height: 32),
+                        if (state is SharedFileIsPrivate) ...[
+                          Text(appLocalizationsOf(context)
+                              .sharedFileIsEncrypted),
+                          const SizedBox(height: 16),
+                          ReactiveForm(
+                            formGroup: context.watch<SharedFileCubit>().form,
+                            child: ReactiveTextField(
+                              formControlName: 'fileKey',
+                              autofocus: true,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText:
+                                    appLocalizationsOf(context).enterFileKey,
+                              ),
+                              validationMessages: kValidationMessages(
+                                  appLocalizationsOf(context)),
+                              onEditingComplete: (_) => context
+                                  .read<SharedFileCubit>()
+                                  .form
+                                  .updateValueAndValidity(),
+                            ),
                           ),
-                          const SizedBox(height: 32),
-                          if (state is SharedFileIsPrivate) ...[
-                            Text(appLocalizationsOf(context)
-                                .sharedFileIsEncrypted),
-                            const SizedBox(height: 16),
-                            ReactiveForm(
-                              formGroup: context.watch<SharedFileCubit>().form,
-                              child: ReactiveTextField(
-                                formControlName: 'fileKey',
-                                autofocus: true,
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  labelText:
-                                      appLocalizationsOf(context).enterFileKey,
-                                ),
-                                validationMessages: kValidationMessages(
-                                    appLocalizationsOf(context)),
-                                onEditingComplete: (_) => context
-                                    .read<SharedFileCubit>()
-                                    .form
-                                    .updateValueAndValidity(),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            ElevatedButton(
-                              onPressed: () =>
-                                  context.read<SharedFileCubit>().submit(),
-                              child: Text(appLocalizationsOf(context).unlock),
-                            ),
-                          ],
-                          if (state is SharedFileLoadInProgress)
-                            const CircularProgressIndicator()
-                          else if (state is SharedFileLoadSuccess) ...{
-                            ListTile(
-                              contentPadding: EdgeInsets.zero,
-                              leading: const Icon(Icons.text_snippet),
-                              title: Text(state.fileRevisions.last.name),
-                              subtitle:
-                                  Text(filesize(state.fileRevisions.last.size)),
-                            ),
-                            const SizedBox(height: 24),
-                            ElevatedButton.icon(
-                              icon: const Icon(Icons.file_download),
-                              label: Text(appLocalizationsOf(context).download),
-                              onPressed: () => promptToDownloadSharedFile(
-                                context: context,
-                                fileId: state.fileRevisions.last.fileId,
-                                fileKey: state.fileKey,
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            _buildReturnToAppLink(context),
-                          } else if (state is SharedFileNotFound) ...{
-                            const Icon(Icons.error_outline, size: 36),
-                            const SizedBox(height: 16),
-                            Text(
-                              appLocalizationsOf(context)
-                                  .specifiedFileDoesNotExist,
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(height: 24),
-                            _buildReturnToAppLink(context),
-                          }
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () =>
+                                context.read<SharedFileCubit>().submit(),
+                            child: Text(appLocalizationsOf(context).unlock),
+                          ),
                         ],
-                      ),
+                        if (state is SharedFileLoadInProgress)
+                          const CircularProgressIndicator()
+                        else if (state is SharedFileLoadSuccess) ...{
+                          ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            leading: const Icon(Icons.text_snippet),
+                            title: Text(state.fileRevisions.last.name),
+                            subtitle:
+                                Text(filesize(state.fileRevisions.last.size)),
+                          ),
+                          const SizedBox(height: 24),
+                          ElevatedButton.icon(
+                            icon: const Icon(Icons.file_download),
+                            label: Text(appLocalizationsOf(context).download),
+                            onPressed: () => promptToDownloadSharedFile(
+                              context: context,
+                              fileId: state.fileRevisions.last.fileId,
+                              fileKey: state.fileKey,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          _buildReturnToAppLink(context),
+                        } else if (state is SharedFileNotFound) ...{
+                          const Icon(Icons.error_outline, size: 36),
+                          const SizedBox(height: 16),
+                          Text(
+                            appLocalizationsOf(context)
+                                .specifiedFileDoesNotExist,
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 24),
+                          _buildReturnToAppLink(context),
+                        }
+                      ],
                     ),
                   ),
                 ),
-              );
+              ),
+            );
+          }
 
-          Widget activityPanel(SharedFileLoadSuccess state) => Card(
-                elevation: 2,
-                child: SharedFileSideSheet(
-                  revisions: state.fileRevisions,
-                  privacy: state.fileKey != null
-                      ? DrivePrivacy.private
-                      : DrivePrivacy.public,
-                ),
-              );
+          Widget activityPanel(SharedFileLoadSuccess state) {
+            return Card(
+              elevation: 2,
+              child: SharedFileSideSheet(
+                revisions: state.fileRevisions,
+                privacy: state.fileKey != null
+                    ? DrivePrivacy.private
+                    : DrivePrivacy.public,
+              ),
+            );
+          }
+
+          final pageHeight = MediaQuery.of(context).size.height;
+
           return Padding(
             padding: const EdgeInsets.all(24.0),
             child: ScreenTypeLayout(
@@ -142,18 +148,19 @@ class SharedFilePage extends StatelessWidget {
                   }
                 ],
               ),
-              mobile: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    shareCard(),
-                    if (state is SharedFileLoadSuccess) ...{
-                      Expanded(
-                        child: activityPanel(state),
+              mobile: ListView(
+                children: [
+                  shareCard(),
+                  if (state is SharedFileLoadSuccess) ...{
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minHeight: 400,
+                        maxHeight: pageHeight / 2,
                       ),
-                    }
-                  ],
-                ),
+                      child: activityPanel(state),
+                    ),
+                  }
+                ],
               ),
             ),
           );
@@ -162,8 +169,10 @@ class SharedFilePage extends StatelessWidget {
     );
   }
 
-  Widget _buildReturnToAppLink(BuildContext context) => TextButton(
-        onPressed: () => launchUrl(Uri.parse('https://ardrive.io/')),
-        child: Text(appLocalizationsOf(context).whatIsArDrive),
-      );
+  Widget _buildReturnToAppLink(BuildContext context) {
+    return TextButton(
+      onPressed: () => launchUrl(Uri.parse('https://ardrive.io/')),
+      child: Text(appLocalizationsOf(context).whatIsArDrive),
+    );
+  }
 }
