4faf655927fcb681e2908aeda25c64dca4d23b1d
Javed Hussein <jdhn42@gmail.com>
2022-08-29 10:13:07 -0400

    refactor: use sized box instead of container PE-1944

--- lib/components/keyboard_handler.dart
+++ lib/components/keyboard_handler.dart
@@ -15,6 +15,7 @@ class KeyboardHandler extends StatefulWidget {
 class _KeyboardHandlerState extends State<KeyboardHandler> {
   final _focusTable = FocusNode();
   var checkboxEnabled = false;
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -22,39 +23,40 @@ class _KeyboardHandlerState extends State<KeyboardHandler> {
       child: BlocBuilder<KeyboardListenerBloc, KeyboardListenerState>(
         builder: (context, state) {
           return RawKeyboardListener(
-              focusNode: _focusTable,
-              autofocus: true,
-              onKey: (event) async {
-                final userAgent =
-                    (await DeviceInfoPlugin().webBrowserInfo).userAgent;
-                late bool ctrlMetaKeyPressed;
-                if (userAgent != null && isApple(userAgent)) {
-                  ctrlMetaKeyPressed =
-                      event.isKeyPressed(LogicalKeyboardKey.metaLeft) ||
-                          event.isKeyPressed(LogicalKeyboardKey.metaRight);
-                } else {
-                  ctrlMetaKeyPressed =
-                      event.isKeyPressed(LogicalKeyboardKey.controlLeft) ||
-                          event.isKeyPressed(LogicalKeyboardKey.controlRight);
-                }
+            focusNode: _focusTable,
+            autofocus: true,
+            onKey: (event) async {
+              final userAgent =
+                  (await DeviceInfoPlugin().webBrowserInfo).userAgent;
+              late bool ctrlMetaKeyPressed;
+              if (userAgent != null && isApple(userAgent)) {
+                ctrlMetaKeyPressed =
+                    event.isKeyPressed(LogicalKeyboardKey.metaLeft) ||
+                        event.isKeyPressed(LogicalKeyboardKey.metaRight);
+              } else {
+                ctrlMetaKeyPressed =
+                    event.isKeyPressed(LogicalKeyboardKey.controlLeft) ||
+                        event.isKeyPressed(LogicalKeyboardKey.controlRight);
+              }
 
-                // detect if ctrl + v or cmd + v is pressed
-                if (ctrlMetaKeyPressed) {
-                  if (event is RawKeyDownEvent) {
-                    setState(() => checkboxEnabled = true);
-                  }
-                } else {
-                  setState(() => checkboxEnabled = false);
+              // detect if ctrl + v or cmd + v is pressed
+              if (ctrlMetaKeyPressed) {
+                if (event is RawKeyDownEvent) {
+                  setState(() => checkboxEnabled = true);
                 }
+              } else {
+                setState(() => checkboxEnabled = false);
+              }
 
-                if (!mounted) return;
-                context.read<KeyboardListenerBloc>().add(
-                      KeyboardListenerUpdateCtrlMetaPressed(
-                        isPressed: checkboxEnabled,
-                      ),
-                    );
-              },
-              child: widget.child);
+              if (!mounted) return;
+              context.read<KeyboardListenerBloc>().add(
+                    KeyboardListenerUpdateCtrlMetaPressed(
+                      isPressed: checkboxEnabled,
+                    ),
+                  );
+            },
+            child: widget.child,
+          );
         },
       ),
     );
