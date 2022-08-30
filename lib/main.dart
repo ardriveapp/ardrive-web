072a8560fb9b9ea7afa0251e27effb3a5375b600
Mati <matibat@gmail.com>
2022-08-30 16:44:38 -0300

    feat(localization ja): adds translations for japanese PE-1465

--- lib/main.dart
+++ lib/main.dart
@@ -107,6 +107,7 @@ class AppState extends State<App> {
                 languageCode: 'zh',
                 countryCode: 'HK',
               ), // generic traditional Chinese 'zh_Hant'
+              Locale('ja', ''), // Japanese, no country code
             ],
             builder: (context, child) => ListTileTheme(
               textColor: kOnSurfaceBodyTextColor,
