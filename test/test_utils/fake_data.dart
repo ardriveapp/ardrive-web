bede10e9a857626319c0f94ffbc898795c29ce74
Karl Prieb <karl@prieb.me>
2022-09-06 14:06:22 -0300

    tests(fake_data): use uuid again

--- test/test_utils/fake_data.dart
+++ test/test_utils/fake_data.dart
@@ -1,5 +1,7 @@
 // Package Info Constants
 
+import 'package:uuid/uuid.dart';
+
 const appName = 'ArDrive-Web';
 const packageName = 'test-package-name';
 const version = 'test=version-1.0';
@@ -12,6 +14,6 @@ const testEntityName = 'test-name';
 const testEntitySize = 10;
 const testEntityDataContentType = 'test-data-mime-type';
 const testEntityDataTxId = 'FxgiSM3JOAsjnZF-Yzv2z7h-Io4K6GmCarhQ0qAUL6I';
-const testEntityId = 'c1dda6a2-0d74-4c35-9671-54e450a778fd';
-const driveId = 'f81f8623-0f59-4bb6-a4a9-e849071ad0a4';
-const rootFolderId = 'df3d39fd-3fb6-4171-8682-bdd946cf629e';
+final testEntityId = const Uuid().v4();
+final driveId = const Uuid().v4();
+final rootFolderId = const Uuid().v4();
