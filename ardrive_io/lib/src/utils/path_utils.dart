c235af49ad070e013d5df6572985e41941980f6a
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2022-09-21 18:49:55 -0300

    fix(wrong extension) save files on downloads directory Android

--- ardrive_io/lib/src/utils/path_utils.dart
+++ ardrive_io/lib/src/utils/path_utils.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:ardrive_io/src/io_exception.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:permission_handler/permission_handler.dart';
 
 /// Returns the folder name to the given path
 ///
@@ -29,24 +30,42 @@ String getDirname(String entityPath) {
 ///
 /// Before usage it needs `Storage` permission
 /// call:
-/// 
+///
 /// ``` dart
 /// await requestPermissions();
 /// await verifyPermissions();
 /// ```
 Future<String> getDefaultMobileDownloadDir() async {
-  if (Platform.isAndroid || Platform.isIOS) {
-    final path = (await path_provider.getApplicationDocumentsDirectory()).path;
-    final downloadDir = Directory(path + '/Downloads/');
+  if (Platform.isAndroid) {
+    final Directory defaultAndroidDownloadDir =
+        Directory('/storage/emulated/0/Download/');
 
-    if (!downloadDir.existsSync()) {
-      downloadDir.createSync();
+    if (await Permission.manageExternalStorage.isGranted &&
+        await defaultAndroidDownloadDir.exists()) {
+      return defaultAndroidDownloadDir.path;
+    } else {
+      final externalDir = await path_provider.getExternalStorageDirectory();
+
+      if (externalDir != null) {
+        return externalDir.path;
+      } else {
+        final directory =
+            await path_provider.getApplicationDocumentsDirectory();
+        return directory.path;
+      }
+    }
+  } else if (Platform.isIOS) {
+    final iosDirectory = await path_provider.getApplicationDocumentsDirectory();
+    final iosDownloadsDirectory = Directory(iosDirectory.path + '/Downloads/');
+
+    if (!iosDownloadsDirectory.existsSync()) {
+      iosDownloadsDirectory.createSync();
     }
 
-    return downloadDir.path;
+    return iosDownloadsDirectory.path;
+  } else {
+    throw UnsupportedPlatformException(
+      'getDefaultMobileDownloadDir only applies to mobile.',
+    );
   }
-
-  throw UnsupportedPlatformException(
-    'getDefaultMobileDownloadDir only applies to mobile.',
-  );
 }
--- ardrive_io/lib/src/utils/permissions.dart
+++ ardrive_io/lib/src/utils/permissions.dart
@@ -4,12 +4,21 @@ import 'package:permission_handler/permission_handler.dart';
 /// Request permissions related to storage on `Android` and `iOS`
 Future<void> requestPermissions() async {
   await Permission.storage.request();
+  await Permission.manageExternalStorage.request();
 }
 
 Future<void> verifyPermissions() async {
+  List<Permission> deniedPErmissions = [];
   if (await Permission.storage.isGranted) {
     return;
+  } else {
+    deniedPErmissions.add(Permission.storage);
+  }
+  if (await Permission.manageExternalStorage.isGranted) {
+    return;
+  } else {
+    deniedPErmissions.add(Permission.manageExternalStorage);
   }
 
-  throw FileSystemPermissionDeniedException([Permission.storage]);
+  throw FileSystemPermissionDeniedException(deniedPErmissions);
 }
