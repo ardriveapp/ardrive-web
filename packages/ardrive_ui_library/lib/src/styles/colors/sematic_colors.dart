cff0b5959c7b9d2b77d0fdc0e86a3a80361b8d00
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2022-11-03 14:55:43 -0300

    feat(design tokens) add colors design tokens PE-2550

---
+++ packages/ardrive_ui_library/lib/src/styles/colors/sematic_colors.dart
@@ -0,0 +1,54 @@
+import 'package:ardrive_ui_library/src/styles/colors/global_colors.dart';
+import 'package:flutter/material.dart';
+
+class ArDriveColors {
+  /// Foreground
+  static const Color themeFgDefault = white;
+  static final Color themeFgMuted = grey.shade100;
+  static final Color themeFgSubtle = grey.shade500;
+  static const Color themeFgOnAccent = white;
+  static final Color themeFgOnDisabled = grey.shade300;
+  static final Color themeFgDisabled = grey.shade600;
+
+  /// Background
+  static final Color themeBgSurface = grey.shade900;
+  static final Color themeGbmuted = grey.shade600;
+  static final Color themeBgSubtle = grey.shade900;
+  static const Color themeBgCanvas = black;
+
+  /// Accent
+  static final Color themeAccentBrand = red.shade500;
+
+  /// Warning
+  static final Color themeErrorFb = red.shade400;
+  static final Color themeErrorMuted = red.shade600;
+  static final Color themeErrorSubtle = red.shade800;
+  static const Color themeErrorOnError = white;
+
+  /// Info
+  static final Color themeInfoFb = blue.shade600;
+  static final Color themeInfoEmphasis = blue.shade500;
+  static final Color themeInfoMuted = blue.shade300;
+  static final Color themeInfoSubtle = blue.shade100;
+  static const Color themeInfoOnInfo = white;
+
+  /// Success
+  static final Color themeSuccessFb = green.shade400;
+  static final Color themeSuccessEmphasis = green.shade500;
+  static final Color themeSuccessMuted = green.shade600;
+  static final Color themeSuccessSubtle = green.shade800;
+  static const Color themeSuccessOnSuccess = white;
+
+  /// Input
+  static const Color themeInputBackground = white;
+  static final Color themeInputText = grey.shade800;
+  static final Color themeInputPlaceholder = grey.shade500;
+  static final Color themeInputBorderDisabled = grey.shade200;
+  static final Color themeInputFbDisabled = grey.shade300;
+
+  /// Border
+  static final Color themeBorderDefault = themeBgSubtle;
+
+  /// Overlay
+  static const Color themeOverlayBackground = black;
+}
