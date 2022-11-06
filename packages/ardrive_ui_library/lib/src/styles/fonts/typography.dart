8516c2a1b02ce7fe68cf837fe62733a94c7af431
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2022-11-06 17:08:39 -0300

    feat(design tokens) implement a few typography design tokens

---
+++ packages/ardrive_ui_library/lib/src/styles/fonts/typography.dart
@@ -0,0 +1,23 @@
+class Typography {
+  // static final
+}
+
+/// Font families
+
+const String fontFamiliesHeadlines = 'Wavehaus';
+const String fontFamiliesBody = 'Wavehaus';
+
+/// Line Height
+const double lineHeightsHeadlinesXl = 1.1;
+const double lineHeightsHeadlinesLg = 1.1;
+const double lineHeightsHeadlinesFefault = 1.1;
+const double lineHeightsHeadlinesSm = 1.3;
+const double lineHeightsBodyRelaxed = 1.75;
+const double lineHeightsBodyDefault = 1.5;
+
+/// Font Sizes
+const int fontSizeBase = 16;
+const double fontSizeScale = 1.2;
+const double fontSizeXxs = fontSizeXs / fontSizeScale;
+const double fontSizeXs = fontSizeSm / fontSizeScale;
+const double fontSizeSm = fontSizeBase / fontSizeScale;
