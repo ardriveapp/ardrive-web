b36dbf1698d407c4973c23add0e078bd593e637c
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2022-11-09 20:05:57 -0300

    add widget book

---
+++ packages/ardrive_ui_library/storybook/lib/src/story_book.dart
@@ -0,0 +1,113 @@
+import 'package:ardrive_ui_library/ardrive_ui_library.dart';
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+import 'colors.dart';
+
+class StoryBook extends StatelessWidget {
+  const StoryBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ArDriveApp(
+      builder: (context) => Widgetbook(
+          themes: [
+            WidgetbookTheme(
+              name: 'Dark',
+              data: darkTheme(),
+            ),
+            WidgetbookTheme(
+              name: 'Light',
+              data: lightMaterialTheme(),
+            ),
+          ],
+          appInfo: AppInfo(name: 'ArDrive StoryBook'),
+          categories: [
+            WidgetbookCategory(name: 'Colors', folders: [
+              getForegroundColors(),
+              getBackgroundColors(),
+              getWarningColors(),
+              getErrorColors(),
+              getInfoColors(),
+              getInputColors(),
+              getSuccessColors(),
+              getOverlayColors(),
+            ]),
+            WidgetbookCategory(name: 'Toggle', widgets: [
+              WidgetbookComponent(name: 'Toggle Dark', useCases: [
+                WidgetbookUseCase(
+                  name: 'On',
+                  builder: (context) {
+                    return const Center(
+                      child: ArDriveToggle(
+                        initialState: ToggleState.on,
+                      ),
+                    );
+                  },
+                ),
+                WidgetbookUseCase(
+                  name: 'Off',
+                  builder: (context) {
+                    return const Center(
+                      child: ArDriveToggle(
+                        initialState: ToggleState.off,
+                      ),
+                    );
+                  },
+                ),
+                WidgetbookUseCase(
+                  name: 'disabled',
+                  builder: (context) {
+                    return const Center(
+                      child: ArDriveToggle(
+                        initialState: ToggleState.disabled,
+                      ),
+                    );
+                  },
+                )
+              ]),
+              WidgetbookComponent(name: 'Toggle Light', useCases: [
+                WidgetbookUseCase(
+                  name: 'On',
+                  builder: (context) {
+                    return Center(
+                      child: ArDriveTheme(
+                        themeData: lightTheme(),
+                        child: const ArDriveToggle(
+                          initialState: ToggleState.on,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                WidgetbookUseCase(
+                  name: 'Off',
+                  builder: (context) {
+                    return Center(
+                      child: ArDriveTheme(
+                        themeData: lightTheme(),
+                        child: const ArDriveToggle(
+                          initialState: ToggleState.off,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                WidgetbookUseCase(
+                  name: 'disabled',
+                  builder: (context) {
+                    return Center(
+                        child: ArDriveTheme(
+                      themeData: lightTheme(),
+                      child: const ArDriveToggle(
+                        initialState: ToggleState.disabled,
+                      ),
+                    ));
+                  },
+                )
+              ])
+            ])
+          ]),
+    );
+  }
+}
