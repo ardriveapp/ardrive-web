1a618f39faf90c12340a1d05c7920f0393ea39c6
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2022-11-18 16:45:31 -0300

    feat(toggle switch) implement toggle switch

---
+++ packages/ardrive_ui_library/lib/src/components/toggle_switch.dart
@@ -0,0 +1,40 @@
+import 'package:ardrive_ui_library/ardrive_ui_library.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ArDriveToggleSwitch extends StatefulWidget {
+  const ArDriveToggleSwitch({
+    super.key,
+    required this.text,
+    this.onChanged,
+    this.value = false,
+    this.isEnabled = true,
+  });
+
+  final String text;
+  final bool value;
+  final bool isEnabled;
+  final Function(bool value)? onChanged;
+
+  @override
+  State<ArDriveToggleSwitch> createState() => _ArDriveToggleSwitchState();
+}
+
+class _ArDriveToggleSwitchState extends State<ArDriveToggleSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ArDriveToggle(
+          initialValue: widget.value,
+          isEnabled: widget.isEnabled,
+          onChanged: widget.onChanged,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Text(widget.text),
+      ],
+    );
+  }
+}
