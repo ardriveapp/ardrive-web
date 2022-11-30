5680fe395ce535fa133cd96baabd4cc7d812f8ae
Javed Hussein <jdhn42@gmail.com>
2022-11-30 14:21:23 -0500

    refactor: add placeholder support PE-2558

--- packages/ardrive_ui_library/lib/src/components/image.dart
+++ packages/ardrive_ui_library/lib/src/components/image.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ArDriveImage extends StatelessWidget {
-  final ImageProvider imageProvider;
-  final bool showPlaceholder;
+  final ImageProvider image;
+  final ImageProvider? placeholder;
+  final double? height;
+  final double? width;
+  final BoxFit? fit;
   const ArDriveImage({
     super.key,
-    required this.imageProvider,
-    this.showPlaceholder = false,
+    required this.image,
+    this.placeholder,
+    this.height,
+    this.width,
+    this.fit = BoxFit.contain,
   });
 
   @override
   Widget build(BuildContext context) {
+    final imagePlaceholder = placeholder;
+    if (imagePlaceholder != null) {
+      return FadeInImage(
+        placeholder: imagePlaceholder,
+        fit: fit,
+        image: image,
+        height: height,
+        width: width,
+      );
+    }
     return Image(
-      fit: BoxFit.contain,
-      image: imageProvider,
+      fit: fit,
+      image: image,
+      height: height,
+      width: width,
     );
   }
 }
--- packages/ardrive_ui_library/storybook/lib/src/image.dart
+++ packages/ardrive_ui_library/storybook/lib/src/image.dart
@@ -1,4 +1,5 @@
 import 'package:ardrive_ui_library/ardrive_ui_library.dart';
+import 'package:flutter/material.dart';
 import 'package:storybook/src/ardrive_app_base.dart';
 import 'package:widgetbook/widgetbook.dart';
 
@@ -10,8 +11,20 @@ WidgetbookCategory image() {
         builder: (context) {
           return ArDriveStorybookAppBase(builder: (context) {
             return ArDriveImage(
-              imageProvider: SvgImage.cachedNetwork(
+              image: SvgImage.cachedNetwork(
                 'https://jovial.com/images/jupiter.svg',
+              ),
+            );
+          });
+        },
+      ),
+      WidgetbookUseCase(
+        name: 'With JPEG Network Image Provider',
+        builder: (context) {
+          return ArDriveStorybookAppBase(builder: (context) {
+            return const ArDriveImage(
+              image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
               ),
             );
           });
