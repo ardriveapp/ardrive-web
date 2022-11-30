634bc2501396ece44699d9dc3006711d3e1a8b65
Mati <matibat@gmail.com>
2022-11-30 19:13:15 -0300

    feat(localization fas): declares Farsi in the main.dart file PE-2727

--- lib/main.dart
+++ lib/main.dart
@@ -189,6 +189,7 @@ class AppState extends State<App> {
                   countryCode: 'HK',
                 ), // generic traditional Chinese 'zh_Hant'
                 Locale('ja', ''), // Japanese, no country code
+                Locale('fas', ''), // Farsi, no country code
               ],
               builder: (context, child) => ListTileTheme(
                 textColor: kOnSurfaceBodyTextColor,
