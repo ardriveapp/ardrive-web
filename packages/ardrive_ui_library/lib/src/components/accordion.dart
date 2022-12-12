e10f1657757428dd446d44567d6dd80b6fbbb290
Javed Hussein <jdhn42@gmail.com>
2022-12-12 13:07:17 -0500

    refactor: remove divider PE-2575

--- packages/ardrive_ui_library/lib/src/components/accordion.dart
+++ packages/ardrive_ui_library/lib/src/components/accordion.dart
@@ -39,32 +39,36 @@ class _ArDriveAccordionState extends State<ArDriveAccordion> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: tiles.map(
-        (tile) {
-          return ExpansionTileTheme(
-            data: ExpansionTileThemeData(
-              backgroundColor:
-                  ArDriveTheme.of(context).themeData.colors.themeBgCanvas,
-              collapsedBackgroundColor:
-                  ArDriveTheme.of(context).themeData.colors.themeBgCanvas,
-              collapsedIconColor:
-                  ArDriveTheme.of(context).themeData.colors.themeAccentBrand,
-              textColor:
-                  ArDriveTheme.of(context).themeData.colors.themeFgDefault,
-              collapsedTextColor:
-                  ArDriveTheme.of(context).themeData.colors.themeFgDefault,
-            ),
-            child: ExpansionTile(
-              title: tile.title,
-              children: tile.children,
-              onExpansionChanged: (value) {
-                setState(() => tiles[tiles.indexOf(tile)].isExpanded = !value);
-              },
-            ),
-          );
-        },
-      ).toList(),
+    return Theme(
+      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+      child: ListView(
+        children: tiles.map(
+          (tile) {
+            return ExpansionTileTheme(
+              data: ExpansionTileThemeData(
+                backgroundColor:
+                    ArDriveTheme.of(context).themeData.colors.themeBgCanvas,
+                collapsedBackgroundColor:
+                    ArDriveTheme.of(context).themeData.colors.themeBgCanvas,
+                collapsedIconColor:
+                    ArDriveTheme.of(context).themeData.colors.themeAccentBrand,
+                textColor:
+                    ArDriveTheme.of(context).themeData.colors.themeFgDefault,
+                collapsedTextColor:
+                    ArDriveTheme.of(context).themeData.colors.themeFgDefault,
+              ),
+              child: ExpansionTile(
+                title: tile.title,
+                children: tile.children,
+                onExpansionChanged: (value) {
+                  setState(
+                      () => tiles[tiles.indexOf(tile)].isExpanded = !value);
+                },
+              ),
+            );
+          },
+        ).toList(),
+      ),
     );
   }
 }
