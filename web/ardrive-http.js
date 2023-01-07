e0188a971ef3c026e72be030f9a5d6e062466c8b
Karl Prieb <karl@ar.io>
2023-01-07 01:02:53 -0300

    update ardrive-http.js file

--- web/ardrive-http.js
+++ web/ardrive-http.js
@@ -1,75 +1,9 @@
-retryStatusCodes = [408, 429, 440, 460, 499, 500, 502, 503, 504, 520, 521, 522, 523, 524, 525, 527, 598, 599];
+'use strict';
 
-function retryDelay(attempt, retryDelayMS) {
-  return parseInt(retryDelayMS * Math.pow(1.5, attempt));
-}
-
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function retry(url, isJson, asBytes, retries, retryDelayMs, noLogs, retryAttempts) {
-  await delay(retryDelay(retryAttempts, retryDelayMs));
-
-  return await get([url, isJson, asBytes, retries - 1, retryDelayMs, noLogs, retryAttempts]);
-}
-
-function formatLog(url, statusCode, statusMessage, retryAttempts) {
-  return `uri: ${url}
+var retryStatusCodes=[408,429,440,460,499,500,502,503,504,520,521,522,523,524,525,527,598,599],isStatusCodeError=code=>code>=400&&code<=599;var logMessage=(url,statusCode,statusMessage,retryAttempts)=>`uri: ${url}
   response: Http status error [${statusCode}]: ${statusMessage}
-  retryAttempts: ${retryAttempts}`;
-}
-
-function isStatusCodeError(code) {
-  return code >= 400 && code <= 599;
-}
-
-async function get([url, isJson, asBytes, retries, retryDelayMs, noLogs, retryAttempts]) {
-  retryAttempts ??= 0;
-
-  try {
-    const response = await fetch(url);
-
-    const statusCode = response.status;
-
-    if (retries > 0 && retryStatusCodes.includes(statusCode)) {
-      const log = formatLog(url, response.status, response.statusText, retryAttempts);
-
-      if (!noLogs) {
-        console.warn(`Network Request Retry\n${log}`);
-      }
-
-      return await retry(url, isJson, asBytes, retries, retryDelayMs, noLogs, retryAttempts + 1);
-    } else {
-      if (isStatusCodeError(statusCode)) {
-        const log = formatLog(url, response.status, response.statusText, retryAttempts);
-
-        return {
-          error: `Network Request Error\n${log}`,
-          retryAttempts,
-        };
-      }
-    }
-
-    let data;
-    if (isJson) {
-      data = await response.json();
-    }
-    if (asBytes) {
-      data = await response.arrayBuffer();
-    }
-
-    if (!(isJson || asBytes)) {
-      data = await response.text();
-    }
-
-    return {
-      statusCode: response.status,
-      statusMessage: response.statusText,
-      data,
-      retryAttempts,
-    };
-  } catch (error) {
-    return { error, retryAttempts };
-  }
-}
+  retryAttempts: ${retryAttempts}`,logger={retry:(url,statusCode,statusMesage,retryAttempts)=>{let standardMessage=logMessage(url,statusCode,statusMesage,retryAttempts);console.warn(`Network Request Retry
+${standardMessage}`);},error:(url,statusCode,statusMesage,retryAttempts)=>{let standardMessage=logMessage(url,statusCode,statusMesage,retryAttempts);console.error(`Network Request Error
+${standardMessage}`);}},requestType={json:{contentType:"application/json; charset=utf-8",getResponse:async response=>await response.json()},bytes:{contentType:"application/octet-stream",getResponse:async response=>await response.arrayBuffer()},text:{contentType:"plain/text; charset=utf-8",getResponse:async response=>await response.text()}},get=async([url,responseType,retries,retryDelayMs,noLogs=!1,retryAttempts=0])=>{try{let response=await fetch(url,{method:"GET",redirect:"follow",signal:AbortSignal.timeout(8e3)}),statusCode=response.status,statusMessage=response.statusText;if(retries>0&&retryStatusCodes.includes(statusCode))return noLogs||logger.retry(url,statusCode,statusMessage,retryAttempts),await get([url,responseType,retries-1,retryDelayMs,noLogs,retryAttempts+1]);if(isStatusCodeError(statusCode))return {error:`Network Request Error
+${logMessage(url,statusCode,statusMessage,retryAttempts)}`,retryAttempts};let data=await requestType[`${responseType}`].getResponse(response);return {statusCode,statusMessage,data,retryAttempts}}catch(error){return {error:`${error}`,retryAttempts}}},post=async([url,data,contentType,responseType,retries,retryDelayMs,noLogs=!1,retryAttempts=0])=>{try{let response=await fetch(url,{method:"POST",headers:{...contentType!==requestType.text.contentType?{"Content-Type":contentType}:{}},redirect:"follow",body:data,signal:AbortSignal.timeout(8e3)}),statusCode=response.status,statusMessage=response.statusText;if(retries>0&&retryStatusCodes.includes(statusCode))return noLogs||logger.retry(url,statusCode,statusMessage,retryAttempts),await post([url,data,contentType,responseType,retries-1,retryDelayMs,noLogs,retryAttempts+1]);if(isStatusCodeError(statusCode))return {error:`Network Request Error
+${logMessage(url,statusCode,statusMessage,retryAttempts)}`,retryAttempts};let responseBody=await requestType[`${responseType}`].getResponse(response);return {statusCode,statusMessage,data:responseBody,retryAttempts}}catch(error){return {error:`${error}`,retryAttempts}}};window.get=get;window.post=post;
