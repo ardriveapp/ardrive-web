cc07ce8026f8cf4e07f86056469128ed52542f70
Mati <matibat@gmail.com>
2023-01-10 09:11:03 -0300

    feat(snapshot to be created): takes a jsonMetadataOfTxId method PE-2814

--- lib/utils/snapshots/snapshot_item_to_be_created.dart
+++ lib/utils/snapshots/snapshot_item_to_be_created.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:ardrive/entities/entities.dart';
+import 'package:ardrive/entities/string_types.dart';
+import 'package:ardrive/services/arweave/graphql/graphql_api.graphql.dart';
 import 'package:ardrive/utils/snapshots/snapshot_types.dart';
 import 'package:ardrive/utils/snapshots/tx_snapshot_to_snapshot_data.dart';
 
-import '../../entities/string_types.dart';
-import '../../services/arweave/graphql/graphql_api.graphql.dart';
 import 'height_range.dart';
 
 typedef DriveHistoryTransaction
@@ -19,26 +19,30 @@ class SnapshotItemToBeCreated {
   final DriveID driveId;
   final Stream<DriveHistoryTransaction> source;
 
+  final Future<String> Function(TxID txId) _jsonMetadataOfTxId;
+
   SnapshotItemToBeCreated({
     required this.blockStart,
     required this.blockEnd,
     required this.driveId,
     required this.subRanges,
     required this.source,
-  });
+    required Future<String> Function(TxID txId) jsonMetadataOfTxId,
+  }) : _jsonMetadataOfTxId = jsonMetadataOfTxId;
 
-  Stream<Uint8List> getSnapshotData() {
-    final txSnapshotStream = source.map<TxSnapshot>(
-      (node) => TxSnapshot(
+  Stream<Uint8List> getSnapshotData() async* {
+    final txSnapshotStream = source.asyncMap<TxSnapshot>(
+      (node) async => TxSnapshot(
         gqlNode: node,
-        jsonMetadata: _isSnapshotTx(node) ? '' : _jsonMetadataOfTxId(node.id),
+        jsonMetadata:
+            _isSnapshotTx(node) ? '' : await _jsonMetadataOfTxId(node.id),
       ),
     );
 
     final snapshotDataStream =
         txSnapshotStream.transform<Uint8List>(txSnapshotToSnapshotData);
 
-    return snapshotDataStream;
+    yield* snapshotDataStream;
   }
 
   bool _isSnapshotTx(DriveHistoryTransaction node) {
@@ -47,10 +51,5 @@ class SnapshotItemToBeCreated {
         tags.where((tag) => tag.name == EntityTag.entityType);
 
     return entityTypeTags.any((tag) => tag.value == 'snapshot');
-  }
-
-  String _jsonMetadataOfTxId(String txId) {
-    // FIXME: let me read the DB, and go to the network if not present.
-    return "TODO";
   }
 }
