edccf2b5fd189e3279eee00b281bd74a43aaf979
Mati <matibat@gmail.com>
2023-01-12 15:49:43 -0300

    feat(snapshot entity): declares SnapshotEntity PE-2824

---
+++ lib/entities/snapshot_entity.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:ardrive/services/services.dart';
+import 'package:arweave/arweave.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'entities.dart';
+
+part 'snapshot_entity.g.dart';
+
+@JsonSerializable()
+class SnapshotEntity extends Entity {
+  @JsonKey(ignore: true)
+  String? id;
+  @JsonKey(ignore: true)
+  String? driveId;
+  @JsonKey(ignore: true)
+  String? blockStart;
+  @JsonKey(ignore: true)
+  String? blockEnd;
+  @JsonKey(ignore: true)
+  String? dataStart;
+  @JsonKey(ignore: true)
+  String? dataEnd;
+
+  SnapshotEntity({
+    this.id,
+    this.driveId,
+    this.blockStart,
+    this.blockEnd,
+    this.dataStart,
+    this.dataEnd,
+  });
+
+  static Future<SnapshotEntity> fromTransaction(
+    TransactionCommonMixin transaction,
+    Uint8List data,
+  ) async {
+    try {
+      Map<String, dynamic>? entityJson = json.decode(utf8.decode(data));
+
+      return SnapshotEntity.fromJson(entityJson!)
+        ..id = transaction.getTag(EntityTag.folderId)
+        ..driveId = transaction.getTag(EntityTag.driveId)
+        ..blockStart = transaction.getTag(EntityTag.blockStart)
+        ..blockEnd = transaction.getTag(EntityTag.blockEnd)
+        ..dataStart = transaction.getTag(EntityTag.dataStart)
+        ..dataEnd = transaction.getTag(EntityTag.dataEnd)
+        ..txId = transaction.id
+        ..ownerAddress = transaction.owner.address
+        ..createdAt = transaction.getCommitTime();
+    } catch (_) {
+      throw EntityTransactionParseException(transactionId: transaction.id);
+    }
+  }
+
+  @override
+  void addEntityTagsToTransaction<T extends TransactionBase>(T tx) {
+    assert(id != null &&
+        driveId != null &&
+        blockStart != null &&
+        blockEnd != null &&
+        dataStart != null &&
+        dataEnd != null);
+
+    tx
+      ..addArFsTag()
+      ..addTag(EntityTag.entityType, EntityType.snapshot)
+      ..addTag(EntityTag.driveId, driveId!)
+      ..addTag(EntityTag.snapshotId, id!)
+      ..addTag(EntityTag.blockStart, blockStart!)
+      ..addTag(EntityTag.blockEnd, blockEnd!)
+      ..addTag(EntityTag.dataStart, dataStart!)
+      ..addTag(EntityTag.dataEnd, dataEnd!);
+  }
+
+  factory SnapshotEntity.fromJson(Map<String, dynamic> json) =>
+      _$SnapshotEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SnapshotEntityToJson(this);
+}
