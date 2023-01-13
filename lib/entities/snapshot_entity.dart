5f91c4811993937e764a5e16b9ac4a321ef7b0b7
Mati <matibat@gmail.com>
2023-01-13 16:30:10 -0300

    feat(snapshot entity): make the height fields to be integer PE-2824

--- lib/entities/snapshot_entity.dart
+++ lib/entities/snapshot_entity.dart
@@ -13,13 +13,13 @@ class SnapshotEntity extends Entity {
   @JsonKey(ignore: true)
   String? driveId;
   @JsonKey(ignore: true)
-  String? blockStart;
+  int? blockStart;
   @JsonKey(ignore: true)
-  String? blockEnd;
+  int? blockEnd;
   @JsonKey(ignore: true)
-  String? dataStart;
+  int? dataStart;
   @JsonKey(ignore: true)
-  String? dataEnd;
+  int? dataEnd;
 
   SnapshotEntity({
     this.id,
@@ -37,10 +37,10 @@ class SnapshotEntity extends Entity {
       return SnapshotEntity()
         ..id = transaction.getTag(EntityTag.snapshotId)
         ..driveId = transaction.getTag(EntityTag.driveId)
-        ..blockStart = transaction.getTag(EntityTag.blockStart)
-        ..blockEnd = transaction.getTag(EntityTag.blockEnd)
-        ..dataStart = transaction.getTag(EntityTag.dataStart)
-        ..dataEnd = transaction.getTag(EntityTag.dataEnd)
+        ..blockStart = int.parse(transaction.getTag(EntityTag.blockStart)!)
+        ..blockEnd = int.parse(transaction.getTag(EntityTag.blockEnd)!)
+        ..dataStart = int.parse(transaction.getTag(EntityTag.dataStart)!)
+        ..dataEnd = int.parse(transaction.getTag(EntityTag.dataEnd)!)
         ..txId = transaction.id
         ..ownerAddress = transaction.owner.address
         ..createdAt = transaction.getCommitTime();
@@ -63,9 +63,9 @@ class SnapshotEntity extends Entity {
       ..addTag(EntityTag.entityType, EntityType.snapshot)
       ..addTag(EntityTag.driveId, driveId!)
       ..addTag(EntityTag.snapshotId, id!)
-      ..addTag(EntityTag.blockStart, blockStart!)
-      ..addTag(EntityTag.blockEnd, blockEnd!)
-      ..addTag(EntityTag.dataStart, dataStart!)
-      ..addTag(EntityTag.dataEnd, dataEnd!);
+      ..addTag(EntityTag.blockStart, '$blockStart')
+      ..addTag(EntityTag.blockEnd, '$blockEnd')
+      ..addTag(EntityTag.dataStart, '$dataStart')
+      ..addTag(EntityTag.dataEnd, '$dataEnd');
   }
 }
