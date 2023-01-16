cf3e6317dce642daab4aa7d1c52b91123525e4c0
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-01-16 09:40:16 -0300

    feat(auth) add authentication API and its implementation

--- lib/authentication/ardrive_auth.dart
+++ lib/authentication/ardrive_auth.dart
@@ -1,42 +1,71 @@
+import 'dart:async';
+
 import 'package:ardrive/entities/constants.dart';
 import 'package:ardrive/services/arweave/arweave.dart';
 import 'package:ardrive/services/crypto/keys.dart';
-import 'package:ardrive/user/services/user_service.dart';
+import 'package:ardrive/user/repositories/user_repository.dart';
 import 'package:ardrive/user/user.dart';
 import 'package:arweave/arweave.dart';
-import 'package:flutter/material.dart';
 
 import '../entities/drive_entity.dart';
 import '../entities/profile_types.dart';
-import '../services/arweave/arweave_service.dart';
 
-class ArDriveAuth extends ChangeNotifier {
-  ArDriveAuth({
+abstract class ArDriveAuth {
+  Future<bool> isUserLoggedIn();
+  Future<bool> isExistingUser(Wallet wallet);
+  Future<User> addUser(Wallet wallet, String password, ProfileType profileType);
+  Future<User> login(Wallet wallet, String password);
+  Future<User> unlockUser({required String password});
+  Future<void> logout();
+  User? get currentUser;
+  Stream<User?> onAuthStateChanged();
+
+  factory ArDriveAuth({
     required ArweaveService arweave,
-    required UserService userService,
-  })  : _arweave = arweave,
-        _userService = userService;
+    required UserRepository userRepository,
+  }) =>
+      _ArDriveAuth(arweave: arweave, userRepository: userRepository);
+}
 
-  final UserService _userService;
+class _ArDriveAuth implements ArDriveAuth {
+  _ArDriveAuth({
+    required ArweaveService arweave,
+    required UserRepository userRepository,
+  })  : _arweave = arweave,
+        _userService = userRepository;
+
+  final UserRepository _userService;
   final ArweaveService _arweave;
 
-  User? _user;
+  User? _currentUser;
 
+  final StreamController<User?> _userController =
+      StreamController<User?>.broadcast();
+
+  @override
   Future<bool> isUserLoggedIn() async {
-    return await _userService.isUserLoggedIn();
+    return await _userService.hasUser();
   }
 
+  @override
   Future<bool> isExistingUser(Wallet wallet) async {
-    return await _userService.isExistingUser(
-      await wallet.getAddress(),
-    );
-  }
-
-  Future<User> login(Wallet wallet, String password) async {
     final driveTxs = await _arweave.getUniqueUserDriveEntityTxs(
       await wallet.getAddress(),
       maxRetries: profileQueryMaxRetries,
     );
+
+    return driveTxs.isNotEmpty;
+  }
+
+  @override
+  Future<User> login(Wallet wallet, String password) async {
+    print('Logging in user');
+    final driveTxs = await _arweave.getUniqueUserDriveEntityTxs(
+      await wallet.getAddress(),
+      maxRetries: profileQueryMaxRetries,
+    );
+
+    print('Found ${driveTxs.length} drive entities');
 
     final privateDriveTxs = driveTxs.where(
         (tx) => tx.getTag(EntityTag.drivePrivacy) == DrivePrivacy.private);
@@ -55,6 +84,8 @@ class ArDriveAuth extends ChangeNotifier {
       DriveEntity? privateDrive;
 
       try {
+        print('Trying to decrypt private drive with id: $checkDriveId');
+
         privateDrive = await _arweave.getLatestDriveEntityWithId(
           checkDriveId,
           checkDriveKey,
@@ -65,6 +96,7 @@ class ArDriveAuth extends ChangeNotifier {
       }
 
       if (privateDrive == null) {
+        print('Could not decrypt private drive with id: $checkDriveId');
         throw AuthenticationFailedException('Incorrect password');
       }
     }
@@ -78,14 +110,20 @@ class ArDriveAuth extends ChangeNotifier {
       wallet,
     );
 
-    return await _userService.getProfile(password);
+    currentUser = await _userService.getUser(password);
+
+    _userController.add(_currentUser);
+
+    // TODO: check if the user has a profile
+    return _currentUser!;
   }
 
+  @override
   Future<User> addUser(
       Wallet wallet, String password, ProfileType profileType) async {
     // delete previous user
     // verify if it is necessary, the user only will add a new user if he is not logged in
-    if (await _userService.isUserLoggedIn()) {
+    if (await _userService.hasUser()) {
       await _userService.deleteUser();
     }
 
@@ -96,33 +134,47 @@ class ArDriveAuth extends ChangeNotifier {
       wallet,
     );
 
-    return await _userService.getProfile(password);
+    currentUser = await _userService.getUser(password);
+
+    _userController.add(_currentUser);
+
+    return _currentUser!;
   }
 
+  @override
   Future<User> unlockUser({required String password}) async {
     try {
-      final user = await _userService.getProfile(password);
+      currentUser = await _userService.getUser(password);
 
-      return user;
+      _userController.add(_currentUser);
+
+      return _currentUser!;
     } catch (e) {
       throw AuthenticationFailedException('Incorrect password');
     }
   }
 
+  @override
   Future<void> logout() async {
+    currentUser = null;
+
+    _userController.add(null);
     // TODO: delete profile
     await _userService.deleteUser();
   }
 
   // getters and setters
-  User? get user => _user;
+  @override
+  User? get currentUser => _currentUser;
 
-  set user(User? val) {
-    if (_user != val) {
-      _user = user;
+  set currentUser(User? val) {
+    if (_currentUser != val) {
+      _currentUser = val;
     }
-    notifyListeners();
   }
+
+  @override
+  Stream<User?> onAuthStateChanged() => _userController.stream;
 }
 
 class AuthenticationFailedException implements Exception {
