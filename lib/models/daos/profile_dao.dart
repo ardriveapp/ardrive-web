5e9e2500dd5fda215af79ac515f981f63ba7f80e
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-02-07 11:24:35 -0300

    wait for deleting profiles

--- lib/models/daos/profile_dao.dart
+++ lib/models/daos/profile_dao.dart
@@ -85,7 +85,8 @@ class ProfileDao extends DatabaseAccessor<Database> with _$ProfileDaoMixin {
 
   Future<void> deleteProfile() async {
     final profile = await defaultProfile().getSingle();
-    (delete(profiles)..where((p) => p.id.equals(profile.id))).go();
+
+    await (delete(profiles)..where((p) => p.id.equals(profile.id))).go();
   }
 
   /// Adds the specified profile and returns a profile key that was used to encrypt the user's wallet
