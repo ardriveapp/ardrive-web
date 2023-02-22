8b194fa0a2a5151e36980e519cfe64f68f951bd9
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-02-22 18:45:47 -0300

    feat(upload limit) add safe limit for public uploads

--- lib/blocs/upload/limits.dart
+++ lib/blocs/upload/limits.dart
@@ -1,3 +1,8 @@
+import 'package:ardrive/utils/data_size.dart';
+
 const privateFileSizeLimit = 104857600;
-const publicFileSizeLimit = 1288490189 * 100; // TODO: Decide a sane limit for stream transctions
+const publicFileSizeLimit =
+    1288490189 * 100; // TODO: Decide a sane limit for stream transctions
 const mobilePrivateFileSizeLimit = 1073741823;
+// 5GiB
+final publicFileSafeSizeLimit = const GiB(5).size;
