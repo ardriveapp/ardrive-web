c59c632968e2d24ad4e27672bc48e736f43431c1
Elliot Sayes <elliot@sayes.cloud>
2023-02-27 18:14:34 +0800

    Update + document new file size limits

--- lib/blocs/upload/limits.dart
+++ lib/blocs/upload/limits.dart
@@ -1,3 +1,17 @@
-const privateFileSizeLimit = 104857600;
-const publicFileSizeLimit = 1288490189 * 100; // TODO: Decide a sane limit for stream transctions
-const mobilePrivateFileSizeLimit = 1073741823;
+// SECURITY WARNING: READ BEFORE INCREASING THIS LIMIT!
+// This must be limited to 64 GiB as the AES Counter bytes are shared with the
+// nonce. Since file revisions reuse the same key, it is critical that the nonce
+// is unique. We assign 12 bytes to the nonce to keep the chance of a collision
+// for random generation acceptably low.
+// This leaves 4 bytes for the block counter, so it can encrypt 2^32 blocks.
+// Since each block is 16 bytes, the total data limit is 64 GiB.
+// In the future, this limit can be increased by updating the key/nonce
+// generation algorithm that so it never reuses the same key/nonce combination.
+const privateFileSizeLimit = 64 * 1024 * 1024 * 1024; // 64 GiB.
+
+// Same as web 
+const mobilePrivateFileSizeLimit = privateFileSizeLimit;
+
+// Set arbitrarily
+const publicFileSizeLimit = 1024 * 1024 * 1024 * 1024; // 1 TiB!
+
