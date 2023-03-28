392a9d380886c30f62e16711a03e3e0309a64db5
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-03-28 09:26:44 -0300

    feat(profile) add profile card

--- lib/components/profile_card.dart
+++ lib/components/profile_card.dart
@@ -1,5 +1,10 @@
+import 'package:ardrive/blocs/profile/profile_cubit.dart';
+import 'package:ardrive/components/details_panel.dart';
+import 'package:ardrive/utils/app_localizations_wrapper.dart';
 import 'package:ardrive_ui/ardrive_ui.dart';
+import 'package:arweave/utils.dart' as utils;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({
@@ -11,26 +16,130 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: ArDriveTheme.of(context).themeData.colors.themeFgDefault,
-          width: 2,
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      builder: (context, state) => state is ProfileLoggedIn
+          ? _loggedInView(context)
+          : _notLoggedInView(context),
+    );
+  }
+
+  Widget _notLoggedInView(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        context.read<ProfileCubit>().logoutProfile();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: ArDriveTheme.of(context).themeData.colors.themeFgDefault,
+            width: 2,
+          ),
+        ),
+        width: 101,
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              appLocalizationsOf(context).login,
+              style: ArDriveTypography.body.buttonNormalBold(),
+            ),
+          ],
         ),
       ),
-      width: 101,
-      height: 40,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ArDriveIcons.person(),
-          Text(
-            '${walletAddress.substring(0, 2)}...${walletAddress.substring(walletAddress.length - 2)}',
-            style: ArDriveTypography.body.buttonNormalBold(),
+    );
+  }
+
+  Widget _loggedInView(BuildContext context) {
+    final state = context.read<ProfileCubit>().state as ProfileLoggedIn;
+    return ArDriveDropdown(
+      width: 250,
+      anchor: const Aligned(
+        follower: Alignment.topRight,
+        target: Alignment.bottomRight,
+      ),
+      items: [
+        ArDriveDropdownItem(
+          content: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if (walletAddress.isNotEmpty)
+                  Text(
+                    '${walletAddress.substring(0, 6)}...${walletAddress.substring(walletAddress.length - 5)}',
+                    style:
+                        ArDriveTypography.body.buttonNormalRegular().copyWith(
+                              decoration: TextDecoration.underline,
+                            ),
+                  ),
+                CopyButton(
+                  text: walletAddress,
+                  showCopyText: false,
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
+        ArDriveDropdownItem(
+          content: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'AR Ballance',
+                  style: ArDriveTypography.body.buttonNormalBold(),
+                ),
+                Text(
+                  '${double.tryParse(utils.winstonToAr(state.walletBalance))?.toStringAsFixed(5) ?? 0} AR',
+                  style: ArDriveTypography.body.buttonNormalBold(),
+                ),
+              ],
+            ),
+          ),
+        ),
+        ArDriveDropdownItem(
+          content: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  appLocalizationsOf(context).logout,
+                  style: ArDriveTypography.body.buttonNormalRegular(),
+                ),
+                ArDriveIcons.logout(),
+              ],
+            ),
+          ),
+          onClick: () => context.read<ProfileCubit>().logoutProfile(),
+        ),
+      ],
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: ArDriveTheme.of(context).themeData.colors.themeFgDefault,
+            width: 2,
+          ),
+        ),
+        width: 101,
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ArDriveIcons.person(),
+            if (walletAddress.isNotEmpty)
+              Text(
+                '${walletAddress.substring(0, 2)}...${walletAddress.substring(walletAddress.length - 2)}',
+                style: ArDriveTypography.body.buttonNormalBold(),
+              ),
+          ],
+        ),
       ),
     );
   }
