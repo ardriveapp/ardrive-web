7d4f01a95384d35c3fe3663b93df6d0598031d26
Mati <matibat@gmail.com>
2023-05-02 16:03:45 -0300

    Revert "feat(migration): placeholder for onUpgradeV16ToV17 PE-3604"
    This reverts commit 7ed318dedb1c45734ae637460bfb533a2c478b21.

--- lib/models/database/migration_strategy/strategies/migration_v16_to_v17.dart
+++ lib/models/database/migration_strategy/strategies/migration_v16_to_v17.dart
@@ -7,6 +7,28 @@ Future<void> onUpgradeV16ToV17(
   int from,
   int to,
 ) async {
-  logger.i('Migrating schema from v16 to v17');
-  throw UnimplementedError('TODO: implement migration from v16 to v17');
+  if (from == 16 && to == 17) {
+    logger.i('Migrating schema from v16 to v17');
+    final driveRevisionsTable = allTables.firstWhere(
+      (element) => element.actualTableName == 'drive_revisions',
+    );
+
+    await m.alterTable(
+      TableMigration(
+        driveRevisionsTable,
+        newColumns: [
+          GeneratedColumn<String?>(
+            'customJsonMetaData',
+            'drive_revisions',
+            true,
+            type: const StringType(),
+            defaultValue: null,
+            clientDefault: null,
+          ),
+        ],
+      ),
+    );
+  } else {
+    throw Exception('Asked to migrate v16 -> v17, but got v$from -> v$to');
+  }
 }
