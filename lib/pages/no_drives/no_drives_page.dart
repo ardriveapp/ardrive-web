a1bf62cce6c86575f068094d902971e68a8923d2
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2023-05-04 12:06:40 -0300

    fix(empty drives) fix empty drives page

--- lib/pages/no_drives/no_drives_page.dart
+++ lib/pages/no_drives/no_drives_page.dart
@@ -1,8 +1,11 @@
-import 'package:ardrive/components/plus_button.dart';
+import 'package:ardrive/app_shell.dart';
+import 'package:ardrive/blocs/drive_detail/drive_detail_cubit.dart';
 import 'package:ardrive/components/profile_card.dart';
+import 'package:ardrive/pages/drive_detail/drive_detail_page.dart';
 import 'package:ardrive/utils/app_localizations_wrapper.dart';
 import 'package:ardrive_ui/ardrive_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 /// A page letting the user know that they have no personal or attached drives
@@ -36,17 +39,24 @@ class NoDrivesPage extends StatelessWidget {
             ],
           ),
         ),
-        mobile: Stack(
-          children: [
-            Center(
-              child: Text(
-                appLocalizationsOf(context).noDrives,
-                textAlign: TextAlign.center,
-                style: ArDriveTypography.headline.headline5Regular(),
+        mobile: Scaffold(
+          bottomNavigationBar: BlocBuilder<DriveDetailCubit, DriveDetailState>(
+            builder: (context, state) {
+              return const CustomBottomNavigation();
+            },
+          ),
+          appBar: const MobileAppBar(),
+          body: Stack(
+            children: [
+              Center(
+                child: Text(
+                  appLocalizationsOf(context).noDrives,
+                  textAlign: TextAlign.center,
+                  style: ArDriveTypography.headline.headline5Regular(),
+                ),
               ),
-            ),
-            const PlusButton(),
-          ],
+            ],
+          ),
         ),
       );
 }
