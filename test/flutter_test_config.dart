afc3df243d571b1bab757cc7f45227a7c41672e7
Javed Hussein <jdhn42@gmail.com>
2023-05-17 11:15:42 -0400

    test: skip platform golden tests

---
+++ test/flutter_test_config.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:alchemist/alchemist.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  return AlchemistConfig.runWithConfig(
+    config: const AlchemistConfig(
+      ciGoldensConfig: CiGoldensConfig(),
+      platformGoldensConfig: PlatformGoldensConfig(enabled: false),
+    ),
+    run: testMain,
+  );
+}
