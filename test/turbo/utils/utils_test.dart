8f9e34cfdbb0bcb63738546b47070be2455129b4
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2023-06-09 07:20:26 -0300

    feat(estimation view): implement a method to abstract the logic to get the literal string from a given credits

---
+++ test/turbo/utils/utils_test.dart
@@ -0,0 +1,22 @@
+import 'package:ardrive/turbo/utils/utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('convertCreditsToLiteralString', () {
+    test('returns correct literal credits string', () {
+      expect(
+          convertCreditsToLiteralString(BigInt.from(5000000000000)), '5.0000');
+      expect(
+          convertCreditsToLiteralString(BigInt.from(1234567890000)), '1.2346');
+      expect(convertCreditsToLiteralString(BigInt.from(0)), '0.0000');
+    });
+  });
+
+  group('convertWinstonToAR', () {
+    test('converts winston to AR as double', () {
+      expect(convertWinstonToAR(BigInt.from(5000000000000)), 5.0);
+      expect(convertWinstonToAR(BigInt.from(1234567890000)), 1.23456789);
+      expect(convertWinstonToAR(BigInt.from(0)), 0.0);
+    });
+  });
+}
