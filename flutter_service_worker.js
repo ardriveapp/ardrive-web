aa6496e87ef96c087b9638fe8cb09ff7fd99e1bf
thiagocarvalhodev <32248947+thiagocarvalhodev@users.noreply.github.com>
2023-07-13 17:34:33 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@daecb60bc61e8991b914df6b8f32861c631f583a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "b508b56e3ef967c9de5e86c1990991c3",
-"/": "b508b56e3ef967c9de5e86c1990991c3",
+const RESOURCES = {"index.html": "f17511a9a4411dc9fb2d336137177d8b",
+"/": "f17511a9a4411dc9fb2d336137177d8b",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432",
 "favicon.png": "167c77d2168cfdb31c240d2d3d5e9601",
@@ -12,7 +12,7 @@ const RESOURCES = {"index.html": "b508b56e3ef967c9de5e86c1990991c3",
 "js/pst.min.js": "353b22b2121953178e78dfe6bce00b6d",
 "js/arconnect.js": "319aaf4ece35e24861e488a1e292c5d7",
 "js/is_document_focused.js": "50c9b2315780823c9234b1115a50d564",
-"main.dart.js": "4ec30afce1eafdc7b534384819d7f4f6",
+"main.dart.js": "e72229154ba7682d5348182d7afc1ec1",
 "version.json": "6b6acbb6aef55a4c03b61291a6da740f",
 "canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
 "canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
