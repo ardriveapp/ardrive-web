f0ba643889e20dcda372630b1f760c04ecf7750e
Mati <matibat@gmail.com>
2023-08-08 16:27:40 -0300

    test(custom gql tags): starts drafting unit tests PE-4352

--- test/entities/custom_json_metadata_test.dart
+++ test/entities/custom_json_metadata_test.dart
@@ -1,24 +1,50 @@
+import 'dart:convert';
+
 import 'package:ardrive/entities/entities.dart';
+import 'package:arweave/arweave.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
+  final fakeFileJson = {
+    // Reserved JSON Metadata
+    'name': 'El archivo',
+    'size': 123,
+    'lastModifiedDate': 12345,
+    'dataTxId': 'txId',
+    'dataContentType': 'text/plain',
+
+    // Custom JSON Metadata
+    'customKey': ['customValue'],
+    'anotherOne': {'papá': 'papa'},
+    'foo': 'bar',
+  };
+  final fakeFolderJson = {
+    // Reserved JSON Metadata
+    'name': 'La carpeta',
+
+    // Custom JSON Metadata
+    'customKey': ['customValue'],
+    'anotherOne': {'papá': 'papa'},
+    'foo': 'bar',
+  };
+  final fakeDriveJson = {
+    // Reserved JSON Metadata
+    'name': 'El disko',
+    'rootFolderId': 'rootFolderId',
+
+    // Custom JSON Metadata
+    'customKey': ['customValue'],
+    'anotherOne': {'papá': 'papa'},
+    'foo': 'bar',
+  };
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('Carry-over of Custom JSON Metadata', () {
     test('for FileEntity', () {
-      final fakeEntityJson = {
-        // Reserved JSON Metadata
-        'name': 'El archivo',
-        'size': 123,
-        'lastModifiedDate': 12345,
-        'dataTxId': 'txId',
-        'dataContentType': 'text/plain',
-
-        // Custom JSON Metadata
-        'customKey': ['customValue'],
-        'anotherOne': {'papá': 'papa'},
-        'foo': 'bar',
-      };
-
-      final parsedEntity = FileEntity.fromJson(fakeEntityJson);
+      final parsedEntity = FileEntity.fromJson(fakeFileJson);
       final customFields = parsedEntity.customJsonMetadata!;
 
       expect(customFields['customKey'], ['customValue']);
@@ -43,17 +69,7 @@ void main() {
     });
 
     test('for FolderEntity', () {
-      final fakeEntityJson = {
-        // Reserved JSON Metadata
-        'name': 'La carpeta',
-
-        // Custom JSON Metadata
-        'customKey': ['customValue'],
-        'anotherOne': {'papá': 'papa'},
-        'foo': 'bar',
-      };
-
-      final parsedEntity = FolderEntity.fromJson(fakeEntityJson);
+      final parsedEntity = FolderEntity.fromJson(fakeFolderJson);
       final customFields = parsedEntity.customJsonMetadata!;
 
       expect(customFields['customKey'], ['customValue']);
@@ -70,18 +86,7 @@ void main() {
     });
 
     test('for DriveEntity', () {
-      final fakeEntityJson = {
-        // Reserved JSON Metadata
-        'name': 'El disko',
-        'rootFolderId': 'rootFolderId',
-
-        // Custom JSON Metadata
-        'customKey': ['customValue'],
-        'anotherOne': {'papá': 'papa'},
-        'foo': 'bar',
-      };
-
-      final parsedEntity = DriveEntity.fromJson(fakeEntityJson);
+      final parsedEntity = DriveEntity.fromJson(fakeDriveJson);
       final customFields = parsedEntity.customJsonMetadata!;
 
       expect(customFields['customKey'], ['customValue']);
@@ -101,6 +106,72 @@ void main() {
   });
 
   group('Carry-over of Custom GQL Tags', () {
-    test('for FileEntity', () {});
+    late Transaction fileTransaction;
+    late Transaction folderTransaction;
+    late Transaction driveTransaction;
+
+    late DataItem fileDataItem;
+    late DataItem folderDataItem;
+    late DataItem driveDataItem;
+
+    late Uint8List fileData;
+    late Uint8List folderData;
+    late Uint8List driveData;
+
+    setUp(() async {
+      PackageInfo.setMockInitialValues(
+        appName: 'appName',
+        packageName: 'packageName',
+        version: '1.2.3',
+        buildNumber: 'buildNumber',
+        buildSignature: 'buildSignature',
+      );
+
+      final FileEntity fileEntity = FileEntity.fromJson(fakeFileJson);
+      final FolderEntity folderEntity = FolderEntity.fromJson(fakeFolderJson);
+      final DriveEntity driveEntity = DriveEntity.fromJson(fakeDriveJson);
+
+      fileEntity.id = '';
+      fileEntity.driveId = '';
+      fileEntity.parentFolderId = '';
+      folderEntity.id = '';
+      folderEntity.driveId = '';
+      folderEntity.parentFolderId = '';
+      driveEntity.id = '';
+      driveEntity.privacy = DrivePrivacy.public;
+
+      fileTransaction = await fileEntity.asTransaction();
+      folderTransaction = await folderEntity.asTransaction();
+      driveTransaction = await driveEntity.asTransaction();
+
+      fileTransaction.addTag('banana', 'manzana');
+      folderTransaction.addTag('banana', 'manzana');
+      driveTransaction.addTag('banana', 'manzana');
+
+      fileDataItem = await fileEntity.asDataItem(null);
+      folderDataItem = await folderEntity.asDataItem(null);
+      driveDataItem = await driveEntity.asDataItem(null);
+
+      fileDataItem.addTag('banana', 'manzana');
+      folderDataItem.addTag('banana', 'manzana');
+      driveDataItem.addTag('banana', 'manzana');
+
+      fileData = Uint8List.fromList(utf8.encode(jsonEncode(fakeFileJson)));
+      folderData = Uint8List.fromList(utf8.encode(jsonEncode(fakeFolderJson)));
+      driveData = Uint8List.fromList(utf8.encode(jsonEncode(fakeDriveJson)));
+    });
+
+    // test('for FileEntity', () async {
+    //   final fileFromTransaction = await FileEntity.fromTransaction(
+    //     /// FIXME: this hack doesn't work
+    //     fileTransaction as TransactionCommonMixin,
+    //     fileData,
+    //     driveKey: null,
+    //     fileKey: null,
+    //     crypto: MockArDriveCrypto(),
+    //   );
+
+    //   expect(fileFromTransaction, isNotNull);
+    // });
   });
 }
