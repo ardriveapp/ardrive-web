1520dbdc7e1e8eadedff828ac7a131934bfafdab
Mati <matibat@gmail.com>
2023-08-08 15:07:12 -0300

    test(custom metadata): adds some failing tests PE-4352

--- test/entities/custom_json_metadata_test.dart
+++ test/entities/custom_json_metadata_test.dart
@@ -1,4 +1,4 @@
-import 'package:ardrive/entities/file_entity.dart';
+import 'package:ardrive/entities/entities.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -13,14 +13,16 @@ void main() {
         'dataContentType': 'text/plain',
 
         // Custom JSON Metadata
-        'customKey': 'customValue',
+        'customKey': ['customValue'],
+        'anotherOne': {'papá': 'papa'},
         'foo': 'bar',
       };
 
       final parsedEntity = FileEntity.fromJson(fakeEntityJson);
       final customFields = parsedEntity.customJsonMetadata!;
 
-      expect(customFields['customKey'], 'customValue');
+      expect(customFields['customKey'], ['customValue']);
+      expect(customFields['anotherOne'], {'papá': 'papa'});
       expect(customFields['foo'], 'bar');
 
       expect(customFields['name'], null);
@@ -35,7 +37,65 @@ void main() {
       expect(serializedEntity['lastModifiedDate'], 12345);
       expect(serializedEntity['dataTxId'], 'txId');
       expect(serializedEntity['dataContentType'], 'text/plain');
-      expect(serializedEntity['customKey'], 'customValue');
+      expect(serializedEntity['customKey'], ['customValue']);
+      expect(serializedEntity['anotherOne'], {'papá': 'papa'});
+      expect(serializedEntity['foo'], 'bar');
+    });
+
+    test('for FolderEntity', () {
+      final fakeEntityJson = {
+        // Reserved JSON Metadata
+        'name': 'La carpeta',
+
+        // Custom JSON Metadata
+        'customKey': ['customValue'],
+        'anotherOne': {'papá': 'papa'},
+        'foo': 'bar',
+      };
+
+      final parsedEntity = FolderEntity.fromJson(fakeEntityJson);
+      final customFields = parsedEntity.customJsonMetadata!;
+
+      expect(customFields['customKey'], ['customValue']);
+      expect(customFields['anotherOne'], {'papá': 'papa'});
+      expect(customFields['foo'], 'bar');
+
+      expect(customFields['name'], null);
+
+      final serializedEntity = parsedEntity.toJson();
+      expect(serializedEntity['name'], 'La carpeta');
+      expect(serializedEntity['customKey'], ['customValue']);
+      expect(serializedEntity['anotherOne'], {'papá': 'papa'});
+      expect(serializedEntity['foo'], 'bar');
+    });
+
+    test('for DriveEntity', () {
+      final fakeEntityJson = {
+        // Reserved JSON Metadata
+        'name': 'El disko',
+        'rootFolderId': 'rootFolderId',
+
+        // Custom JSON Metadata
+        'customKey': ['customValue'],
+        'anotherOne': {'papá': 'papa'},
+        'foo': 'bar',
+      };
+
+      final parsedEntity = DriveEntity.fromJson(fakeEntityJson);
+      final customFields = parsedEntity.customJsonMetadata!;
+
+      expect(customFields['customKey'], ['customValue']);
+      expect(customFields['anotherOne'], {'papá': 'papa'});
+      expect(customFields['foo'], 'bar');
+
+      expect(customFields['name'], null);
+      expect(customFields['rootFolderId'], null);
+
+      final serializedEntity = parsedEntity.toJson();
+      expect(serializedEntity['name'], 'El drive');
+      expect(serializedEntity['rootFolderId'], 'rootFolderId');
+      expect(serializedEntity['customKey'], ['customValue']);
+      expect(serializedEntity['anotherOne'], {'papá': 'papa'});
       expect(serializedEntity['foo'], 'bar');
     });
   });
