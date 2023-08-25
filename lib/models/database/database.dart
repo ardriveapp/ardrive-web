e6084aa0e1ad428a54402f7dcc5efbc15264c77d
Mati <matibat@gmail.com>
2023-08-25 15:33:18 -0300

    feat(db migration): fixes error thrown on DB Migration PE-4487

--- lib/models/database/database.dart
+++ lib/models/database/database.dart
@@ -30,186 +30,26 @@ class Database extends _$Database {
             // Then we're adding the pin and custom fields columns
             logger.i('Migrating schema from v16 to v17');
 
-            final driveRevisionsTable = allTables.firstWhere(
-              (element) => element.actualTableName == 'drive_revisions',
+            await m.addColumn(
+              driveRevisions,
+              driveRevisions.customJsonMetadata,
             );
-            final driveEntriesTable = allTables.firstWhere(
-              (element) => element.actualTableName == 'drives',
-            );
+            await m.addColumn(driveRevisions, driveRevisions.customGQLTags);
+            await m.addColumn(drives, drives.customJsonMetadata);
+            await m.addColumn(drives, drives.customGQLTags);
 
-            await m.alterTable(
-              TableMigration(
-                driveRevisionsTable,
-                newColumns: [
-                  GeneratedColumn(
-                    'customJsonMetadata',
-                    'drive_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'customGQLTags',
-                    'drive_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                ],
-              ),
+            await m.addColumn(
+              folderRevisions,
+              folderRevisions.customJsonMetadata,
             );
+            await m.addColumn(folderRevisions, folderRevisions.customGQLTags);
+            await m.addColumn(folderEntries, folderEntries.customJsonMetadata);
+            await m.addColumn(folderEntries, folderEntries.customGQLTags);
 
-            await m.alterTable(
-              TableMigration(
-                driveEntriesTable,
-                newColumns: [
-                  GeneratedColumn(
-                    'customJsonMetadata',
-                    'drives',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'customGQLTags',
-                    'drive',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                ],
-              ),
-            );
-
-            final folderRevisionsTable = allTables.firstWhere(
-              (element) => element.actualTableName == 'folder_revisions',
-            );
-            final folderEntriesTable = allTables.firstWhere(
-              (element) => element.actualTableName == 'folder_entries',
-            );
-
-            await m.alterTable(
-              TableMigration(
-                folderRevisionsTable,
-                newColumns: [
-                  GeneratedColumn(
-                    'customJsonMetadata',
-                    'folder_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'customGQLTags',
-                    'folder_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                ],
-              ),
-            );
-
-            await m.alterTable(
-              TableMigration(
-                folderEntriesTable,
-                newColumns: [
-                  GeneratedColumn(
-                    'customJsonMetadata',
-                    'folder_entries',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'customGQLTags',
-                    'folder_entries',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                ],
-              ),
-            );
-
-            final fileRevisionsTable = allTables.firstWhere(
-              (element) => element.actualTableName == 'file_revisions',
-            );
-            final fileEntriesTable = allTables.firstWhere(
-              (element) => element.actualTableName == 'file_entries',
-            );
-
-            await m.alterTable(
-              TableMigration(
-                fileRevisionsTable,
-                newColumns: [
-                  GeneratedColumn(
-                    'customJsonMetadata',
-                    'file_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'customGQLTags',
-                    'file_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'pinnedDataOwnerAddress',
-                    'file_revisions',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                ],
-              ),
-            );
-
-            await m.alterTable(
-              TableMigration(
-                fileEntriesTable,
-                newColumns: [
-                  GeneratedColumn(
-                    'customJsonMetadata',
-                    'file_entries',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'customGQLTags',
-                    'file_entries',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                  GeneratedColumn(
-                    'pinnedDataOwnerAddress',
-                    'file_entries',
-                    true,
-                    type: DriftSqlType.string,
-                    defaultValue: null,
-                    clientDefault: null,
-                  ),
-                ],
-              ),
-            );
+            await m.addColumn(fileRevisions, fileRevisions.customJsonMetadata);
+            await m.addColumn(fileRevisions, fileRevisions.customGQLTags);
+            await m.addColumn(fileEntries, fileEntries.customJsonMetadata);
+            await m.addColumn(fileEntries, fileEntries.customGQLTags);
           } else if (from >= 1 && from < schemaVersion) {
             // Reset the database.
             for (final table in allTables) {
