2752e38534935c71cebb2dda96a3705a78fef1bd
Mati <matibat@gmail.com>
2023-09-26 13:26:36 -0300

    feat(app shell): makes it not to show the wallet switch dialog when not using arconnect PE-4477

--- lib/app_shell.dart
+++ lib/app_shell.dart
@@ -2,6 +2,7 @@ import 'package:ardrive/components/profile_card.dart';
 import 'package:ardrive/components/side_bar.dart';
 import 'package:ardrive/pages/drive_detail/components/hover_widget.dart';
 import 'package:ardrive/utils/html/html_util.dart';
+import 'package:ardrive/utils/logger/logger.dart';
 import 'package:ardrive/utils/size_constants.dart';
 import 'package:ardrive_ui/ardrive_ui.dart';
 import 'package:flutter/material.dart';
@@ -31,17 +32,27 @@ class AppShellState extends State<AppShell> {
   bool _showProfileOverlay = false;
   bool _showWalletSwitchDialog = true;
   @override
-  Widget build(BuildContext context) => BlocBuilder<DrivesCubit, DrivesState>(
+  Widget build(BuildContext context) => BlocBuilder<ProfileCubit, ProfileState>(
         builder: (context, state) {
           onArConnectWalletSwitch(() {
-            if (_showWalletSwitchDialog) {
-              showDialog(
-                context: context,
-                builder: (context) => const WalletSwitchDialog(),
-              );
-            }
-            //Used to prevent the dialog being shown multiple times.
-            _showWalletSwitchDialog = false;
+            context
+                .read<ProfileCubit>()
+                .isCurrentProfileArConnect()
+                .then((isCurrentProdileArConnect) {
+              if (_showWalletSwitchDialog) {
+                if (isCurrentProdileArConnect) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const WalletSwitchDialog(),
+                  );
+                } else {
+                  logger.d('Wallet switch detected while not logged in'
+                      ' to ArConnect. Ignoring.');
+                }
+              }
+              //Used to prevent the dialog being shown multiple times.
+              _showWalletSwitchDialog = false;
+            });
           });
 
           Widget buildPage(scaffold) => Material(
