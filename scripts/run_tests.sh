4393feb3dd7b4ea6783e0c81720c3722b669b9fd
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-11-14 09:35:42 -0300

    chore: adds tests for all packages in the workflow

---
+++ scripts/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "Running tests in main app"
+flutter pub get
+flutter test
+
+echo "Searching for packages..."
+for dir in packages/*; do
+  if [ -d "$dir" ]; then
+    echo "Checking $dir"
+    if [ -d "$dir/test" ]; then
+      echo "Running tests in $dir"
+      cd $dir
+      flutter pub get
+      flutter test
+      cd ../..
+    else
+      echo "No test directory found in $dir, skipping tests"
+    fi
+  fi
+done
