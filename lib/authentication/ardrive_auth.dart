71fe9407ac2c9ff16f7ede225d7e40075f61e86f
Mati <matibat@gmail.com>
2023-11-27 16:19:20 -0300

    feat(ardrive auth): await for methods called by logout PE-4943

--- lib/authentication/ardrive_auth.dart
+++ lib/authentication/ardrive_auth.dart
@@ -227,7 +227,7 @@ class ArDriveAuthImpl implements ArDriveAuth {
       }
 
       await _databaseHelpers.deleteAllTables();
-      (await _metadataCache).clear();
+      await (await _metadataCache).clear();
     } catch (e) {
       logger.e('Failed to logout user', e);
       throw AuthenticationFailedException('Failed to logout user');
