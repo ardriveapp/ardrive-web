5b36c97adca651bd3088c4304c99ae3731a13b1f
Elliot Sayes <elliot@sayes.cloud>
2023-12-11 15:53:04 +0700

    Template for fs_entry_license (based on fs_entry_move)

---
+++ lib/blocs/fs_entry_license/fs_entry_license_event.dart
@@ -0,0 +1,46 @@
+part of 'fs_entry_license_bloc.dart';
+
+abstract class FsEntryMoveEvent extends Equatable {
+  const FsEntryMoveEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FsEntryMoveInitial extends FsEntryMoveEvent {
+  const FsEntryMoveInitial() : super();
+}
+
+class FsEntryMoveUpdateTargetFolder extends FsEntryMoveEvent {
+  final String folderId;
+  const FsEntryMoveUpdateTargetFolder({required this.folderId}) : super();
+  @override
+  List<Object> get props => [folderId];
+}
+
+class FsEntryMoveGoBackToParent extends FsEntryMoveEvent {
+  final FolderEntry folderInView;
+  const FsEntryMoveGoBackToParent({required this.folderInView}) : super();
+  @override
+  List<Object> get props => [folderInView];
+}
+
+class FsEntryMoveSubmit extends FsEntryMoveEvent {
+  final FolderEntry folderInView;
+  const FsEntryMoveSubmit({
+    required this.folderInView,
+  }) : super();
+  @override
+  List<Object> get props => [folderInView];
+}
+
+class FsEntryMoveSkipConflicts extends FsEntryMoveEvent {
+  final FolderEntry folderInView;
+  final List<ArDriveDataTableItem> conflictingItems;
+  const FsEntryMoveSkipConflicts({
+    required this.folderInView,
+    required this.conflictingItems,
+  }) : super();
+  @override
+  List<Object> get props => [folderInView, conflictingItems];
+}
---
+++ lib/blocs/fs_entry_license/fs_entry_license_state.dart
@@ -0,0 +1,64 @@
+part of 'fs_entry_license_bloc.dart';
+
+abstract class FsEntryMoveState extends Equatable {
+  const FsEntryMoveState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FsEntryMoveLoadInProgress extends FsEntryMoveState {
+  const FsEntryMoveLoadInProgress() : super();
+}
+
+class FsEntryMoveLoadSuccess extends FsEntryMoveState {
+  final bool viewingRootFolder;
+  final FolderWithContents viewingFolder;
+
+  /// The id of the folder/file entry being moved.
+  final List<ArDriveDataTableItem> itemsToMove;
+
+  const FsEntryMoveLoadSuccess({
+    required this.viewingRootFolder,
+    required this.viewingFolder,
+    required this.itemsToMove,
+  }) : super();
+  @override
+  List<Object> get props => [viewingRootFolder, viewingFolder, itemsToMove];
+}
+
+class FsEntryMoveWalletMismatch extends FsEntryMoveState {
+  const FsEntryMoveWalletMismatch() : super();
+}
+
+class FsEntryMoveSuccess extends FsEntryMoveState {
+  const FsEntryMoveSuccess() : super();
+}
+
+class FsEntryMoveNameConflict extends FsEntryMoveState {
+  final List<ArDriveDataTableItem> conflictingItems;
+  final FolderEntry folderInView;
+
+  final List<ArDriveDataTableItem> allItems;
+
+  const FsEntryMoveNameConflict({
+    required this.conflictingItems,
+    required this.folderInView,
+    required this.allItems,
+  }) : super();
+
+  bool areAllItemsConflicting() => conflictingItems.length == allItems.length;
+
+  List<String> conflictingFileNames() => conflictingItems
+      .whereType<SelectedFile>()
+      .map((e) => e.item.name)
+      .toList();
+
+  List<String> conflictingFolderNames() => conflictingItems
+      .whereType<SelectedFolder>()
+      .map((e) => e.item.name)
+      .toList();
+
+  @override
+  List<Object> get props => [conflictingItems, folderInView];
+}
