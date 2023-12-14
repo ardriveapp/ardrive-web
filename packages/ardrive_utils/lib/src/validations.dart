e8944cb2087375d4ec33f1d1618d41d0f8a3be9f
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-12-14 10:00:40 -0300

    test(uuid validator)
    implement unit tests for uuid v4 validation

--- packages/ardrive_utils/lib/src/validations.dart
+++ packages/ardrive_utils/lib/src/validations.dart
@@ -1,7 +1,6 @@
 bool isValidUuidV4(String uuid) {
   final RegExp uuidV4Pattern = RegExp(
-    r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
-  );
+      r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$');
 
-  return uuidV4Pattern.hasMatch(uuid);
+  return uuidV4Pattern.hasMatch(uuid.toLowerCase());
 }
