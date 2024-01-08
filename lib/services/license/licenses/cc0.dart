436bc4e0496b75d8f2e7672e315db6d5e5e280bc
Elliot Sayes <elliot@sayes.cloud>
2024-01-08 19:12:58 +0800

    Oops

--- lib/services/license/licenses/cc0.dart
+++ lib/services/license/licenses/cc0.dart
@@ -1,7 +1,7 @@
 import '../license_types.dart';
 
 const ccByLicenseMeta = LicenseMeta(
-  licenseType: LicenseType.udl,
+  licenseType: LicenseType.ccBy,
   licenseDefinitionTxId: 'rz2DNzn9pnYOU6049Wm6V7kr0BhyfWE6ZD_mqrXMv5A',
   name: 'Creative Commons Attribution',
   shortName: 'CC-BY',
