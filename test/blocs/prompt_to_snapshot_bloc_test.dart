497bc0fc1c2ccd84e98a4d23090db55f3bfb8e30
Mati <matibat@gmail.com>
2024-01-11 15:21:47 -0300

    test(prompt to snapshot): updates unit test PE-5254

--- test/blocs/prompt_to_snapshot_bloc_test.dart
+++ test/blocs/prompt_to_snapshot_bloc_test.dart
@@ -1,16 +1,22 @@
+import 'package:ardrive/blocs/profile/profile_cubit.dart';
 import 'package:ardrive/blocs/prompt_to_snapshot/prompt_to_snapshot_bloc.dart';
 import 'package:ardrive/blocs/prompt_to_snapshot/prompt_to_snapshot_event.dart';
 import 'package:ardrive/blocs/prompt_to_snapshot/prompt_to_snapshot_state.dart';
+import 'package:ardrive/models/daos/daos.dart';
+import 'package:ardrive/models/database/database.dart';
 import 'package:ardrive/user/repositories/user_repository.dart';
 import 'package:ardrive/utils/key_value_store.dart';
 import 'package:ardrive/utils/local_key_value_store.dart';
+import 'package:ardrive_utils/ardrive_utils.dart';
+import 'package:arweave/arweave.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:cryptography/cryptography.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../test_utils/mocks.dart';
+import '../test_utils/utils.dart';
 
 const durationBeforePrompting = Duration(milliseconds: 200);
 const numberOfTxsBeforeSnapshot = 3;
@@ -20,11 +26,17 @@ void main() {
   late PromptToSnapshotBloc promptToSnapshotBloc;
   late KeyValueStore store;
   late UserRepository userRepository;
+  late ProfileCubit profileCubit;
+  late Database db;
+  late DriveDao driveDao;
+  late Wallet testWallet;
+  late String walletAddress;
+  late DriveID driveId;
 
   WidgetsFlutterBinding.ensureInitialized();
 
   group('PromptToSnapshotBloc class', () {
-    setUp(() async {
+    setUpAll(() async {
       SharedPreferences.setMockInitialValues({});
       final fakePrefs = await SharedPreferences.getInstance();
       store = await LocalKeyValueStore.getInstance(prefs: fakePrefs);
@@ -33,11 +45,42 @@ void main() {
       when(() => userRepository.getOwnerOfDefaultProfile())
           .thenAnswer((_) => Future.value('test-owner'));
 
+      testWallet = getTestWallet();
+      walletAddress = await testWallet.getAddress();
+
+      profileCubit = MockProfileCubit();
+      when(() => profileCubit.state).thenReturn(ProfileLoggedIn(
+        username: 'test-username',
+        password: 'test-password',
+        wallet: testWallet,
+        walletAddress: walletAddress,
+        walletBalance: BigInt.one,
+        cipherKey: SecretKey(List.generate(32, (index) => index)),
+        useTurbo: false,
+      ));
+
+      db = getTestDb();
+      driveDao = db.driveDao;
+
+      final drive = await driveDao.createDrive(
+        name: "Mati's drive",
+        ownerAddress: walletAddress,
+        privacy: 'public',
+        wallet: testWallet,
+        password: '123',
+        profileKey: SecretKey([1, 2, 3, 4, 5]),
+      );
+      driveId = drive.driveId;
+    });
+
+    setUp(() {
       promptToSnapshotBloc = PromptToSnapshotBloc(
         store: store,
         durationBeforePrompting: durationBeforePrompting,
         numberOfTxsBeforeSnapshot: numberOfTxsBeforeSnapshot,
         userRepository: userRepository,
+        profileCubit: profileCubit,
+        driveDao: driveDao,
       );
     });
 
@@ -45,17 +88,17 @@ void main() {
       'can count through txs',
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
-        bloc.add(const CountSyncedTxs(
+        bloc.add(CountSyncedTxs(
           driveId: driveId,
           txsSyncedWithGqlCount: 1,
           wasDeepSync: false,
         ));
-        bloc.add(const CountSyncedTxs(
+        bloc.add(CountSyncedTxs(
           driveId: driveId,
           txsSyncedWithGqlCount: 2,
           wasDeepSync: false,
         ));
-        bloc.add(const CountSyncedTxs(
+        bloc.add(CountSyncedTxs(
           driveId: driveId,
           txsSyncedWithGqlCount: 3,
           wasDeepSync: false,
@@ -70,12 +113,12 @@ void main() {
       'will prompt to snapshot after enough txs',
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
       },
       expect: () => [
-        const PromptToSnapshotPrompting(driveId: driveId),
+        PromptToSnapshotPrompting(driveId: driveId),
       ],
     );
 
@@ -83,7 +126,14 @@ void main() {
       'will not prompt to snapshot if drive is deselected',
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
+
+        // This delay sumulates the user moving the cursor to a different drive.
+        /// Without this delay, the test will fail because the bloc will
+        /// first handle the event for nulling the driveId, and then handle
+        /// the event for selecting the driveId.
+        await Future<void>.delayed(const Duration(milliseconds: 1));
+
         bloc.add(const SelectedDrive(driveId: null));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
@@ -97,13 +147,13 @@ void main() {
       'selecting an already snapshotted drive does nothing',
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
-        bloc.add(const DriveSnapshotted(driveId: driveId));
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(DriveSnapshotted(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
       },
       expect: () => [
-        const PromptToSnapshotIdle(driveId: driveId),
+        PromptToSnapshotIdle(driveId: driveId),
       ],
     );
 
@@ -112,7 +162,7 @@ void main() {
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
         bloc.add(const DismissDontAskAgain(dontAskAgain: false));
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
       },
@@ -126,7 +176,7 @@ void main() {
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
         bloc.add(const DismissDontAskAgain(dontAskAgain: true));
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
       },
@@ -141,7 +191,7 @@ void main() {
       act: (PromptToSnapshotBloc bloc) async {
         when(() => userRepository.getOwnerOfDefaultProfile())
             .thenAnswer((_) => Future.value(null));
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
       },
@@ -152,7 +202,29 @@ void main() {
       'selecting a drive while sync is running does nothing',
       build: () => promptToSnapshotBloc,
       act: (PromptToSnapshotBloc bloc) async {
-        bloc.add(const SelectedDrive(driveId: driveId));
+        bloc.add(SelectedDrive(driveId: driveId));
+        const durationAfterPrompting = Duration(milliseconds: 250);
+        await Future<void>.delayed(durationAfterPrompting);
+      },
+      expect: () => [],
+    );
+
+    blocTest(
+      'selecting a drive whith no write permissions does nothing',
+      build: () => promptToSnapshotBloc,
+      act: (PromptToSnapshotBloc bloc) async {
+        final aDifferentWallet = getTestWallet();
+        final aDifferentWalletAddress = await aDifferentWallet.getAddress();
+        when(() => profileCubit.state).thenReturn(ProfileLoggedIn(
+          username: 'another-test-username',
+          password: 'another-test-password',
+          wallet: aDifferentWallet,
+          walletAddress: aDifferentWalletAddress,
+          walletBalance: BigInt.one,
+          cipherKey: SecretKey(List.generate(32, (index) => index)),
+          useTurbo: false,
+        ));
+        bloc.add(SelectedDrive(driveId: driveId));
         const durationAfterPrompting = Duration(milliseconds: 250);
         await Future<void>.delayed(durationAfterPrompting);
       },
