c167e5f43f196cd38475b40c1a4a8b3cb46e4c3a
Karl Prieb <karl@ar.io>
2024-01-19 11:17:00 -0300

    Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,6 @@ import 'package:ardrive/utils/pre_cache_assets.dart';
 import 'package:ardrive/utils/secure_key_value_store.dart';
 import 'package:ardrive_http/ardrive_http.dart';
 import 'package:ardrive_io/ardrive_io.dart';
-import 'package:ardrive_logger/ardrive_logger.dart';
 import 'package:ardrive_ui/ardrive_ui.dart';
 import 'package:ardrive_utils/ardrive_utils.dart';
 import 'package:arweave/arweave.dart';
