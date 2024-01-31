c6e22ae2eea555d58c57a3cd9a69f988fc440a96
Atticus <atticusofsparta@protonmail.com>
2024-01-31 14:06:50 -0600

    fix(lint): change forEach function type

--- lib/entities/license_assertion.dart
+++ lib/entities/license_assertion.dart
@@ -87,7 +87,7 @@ class LicenseAssertionEntity with TransactionPropertiesMixin {
       ...additionalTags.entries
     ];
 
-    tags.forEach((tag) => licenseAssertionDataItem.addTag(tag.key, tag.value));
+    tags.forEach((tag) { licenseAssertionDataItem.addTag(tag.key, tag.value) });
 
     return licenseAssertionDataItem;
   }
