29cf89a8b901a5e77efcc384b4272ac891de20b5
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2024-02-22 15:57:15 -0300

    Update cc_by.dart

--- lib/services/license/licenses/cc_by.dart
+++ lib/services/license/licenses/cc_by.dart
@@ -11,7 +11,7 @@ List<LicenseMeta> ccLicenses = [
 
 const ccByLicenseMeta = LicenseMeta(
   licenseType: LicenseType.ccBy,
-  licenseDefinitionTxId: 'rz2DNzn9pnYOU6049Wm6V7kr0BhyfWE6ZD_mqrXMv5A',
+  licenseDefinitionTxId: 'mSOFUrl5mUQvG7VBP36DD39kzJASv9FDe3GxHpcCvRA',
   name: 'Attribution',
   shortName: 'CC-BY',
   version: '4.0',
