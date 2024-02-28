ee0df03490918b9e0b9c434d34a60161dca8a8a1
Elliot Sayes <elliot@sayes.cloud>
2024-02-28 14:45:31 +0800

    Rename cc_by => cc

--- lib/models/forms/cc.dart
+++ lib/models/forms/cc.dart
@@ -1,5 +1,5 @@
 import 'package:ardrive/services/license/license_state.dart';
-import 'package:ardrive/services/license/licenses/cc_by.dart';
+import 'package:ardrive/services/license/licenses/cc.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 createCcForm() => FormGroup({
