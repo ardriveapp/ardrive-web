2b2e5c97080d9ba535dc471332885f4d9f44241c
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2024-04-16 10:14:29 -0300

    fix lint

--- lib/authentication/login/views/tiles/tiles_view.dart
+++ lib/authentication/login/views/tiles/tiles_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, deprecated_member_use
 
 import 'package:ardrive/authentication/components/breakpoint_layout_builder.dart';
 import 'package:ardrive/misc/misc.dart';
