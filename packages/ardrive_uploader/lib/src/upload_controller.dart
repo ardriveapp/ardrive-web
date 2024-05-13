6758cad197c5ceb170fa908e20135fb463f95088
Thiago Carvalho <32248947+thiagocarvalhodev@users.noreply.github.com>
2024-05-13 16:37:00 -0300

    Update upload_controller.dart

--- packages/ardrive_uploader/lib/src/upload_controller.dart
+++ packages/ardrive_uploader/lib/src/upload_controller.dart
@@ -391,17 +391,11 @@ class _UploadController implements UploadController {
 
   void Function(UploadProgress progress)? _onProgressChange = (progress) {};
 
-  void Function(List<UploadTask> tasks) _onDone = (List<UploadTask> tasks) {
-    print('Upload Finished');
-  };
+  void Function(List<UploadTask> tasks) _onDone = (List<UploadTask> tasks) {};
 
-  void Function(List<UploadTask> tasks) _onCancel = (List<UploadTask> tasks) {
-    print('Upload Canceled');
-  };
+  void Function(List<UploadTask> tasks) _onCancel = (List<UploadTask> tasks) {};
 
-  void Function(List<UploadTask> tasks) _onError = (List<UploadTask> tasks) {
-    print('Upload Error');
-  };
+  void Function(List<UploadTask> tasks) _onError = (List<UploadTask> tasks) {};
 
   void Function(UploadTask task) _onCompleteTask = (UploadTask tasks) {};
 
