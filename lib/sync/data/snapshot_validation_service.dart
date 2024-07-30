86485034df4c5b9e424c7f22c0b0ad281671f863
Thiago Carvalho <32248947+thiagocarvalhodev@users.noreply.github.com>
2024-07-30 11:03:29 -0300

    feat(snapshots)
    - validate snapshot existence before syncing from snapshots

---
+++ lib/sync/data/snapshot_validation_service.dart
@@ -0,0 +1,66 @@
+import 'package:ardrive/services/config/config_service.dart';
+import 'package:ardrive/utils/logger.dart';
+import 'package:ardrive/utils/snapshots/snapshot_item.dart';
+import 'package:http/http.dart' as http;
+
+class SnapshotValidationService {
+  final ConfigService _configService;
+
+  SnapshotValidationService({
+    required ConfigService configService,
+  }) : _configService = configService;
+
+  Future<List<SnapshotItem>> validateSnapshotItems(
+    List<SnapshotItem> snapshotItems,
+  ) async {
+    List<SnapshotItem> snapshotsVerified = [];
+
+    final futures = snapshotItems.map((snapshotItem) async {
+      final appConfig = _configService.config;
+
+      try {
+        final headers = {'Accept': '*/*'};
+
+        final snapshotValidation = await http.head(
+          Uri.parse(
+              '${appConfig.defaultArweaveGatewayUrl}/${snapshotItem.txId}'),
+          headers: headers,
+        );
+
+        logger.d('Validating snapshot ${snapshotItem.txId}');
+
+        if (snapshotValidation.statusCode == 200) {
+          if (snapshotValidation.headers['content-length'] != null) {
+            int lenght =
+                int.parse(snapshotValidation.headers['content-length']!);
+
+            final headers = {
+              'Range': 'bytes=${lenght - 8}-$lenght',
+            };
+
+            final validationRequest = await http.get(
+              Uri.parse(
+                  '${appConfig.defaultArweaveGatewayUrl}${snapshotItem.txId}'),
+              headers: headers,
+            );
+
+            logger.d(
+                'Validation request status code: ${validationRequest.statusCode}');
+          }
+          logger.d('Snapshot ${snapshotItem.txId} is valid');
+
+          snapshotsVerified.add(snapshotItem);
+        }
+      } catch (e) {
+        logger.w('Error while validating snapshot. ${snapshotItem.txId}');
+      }
+    });
+
+    await Future.wait(futures);
+
+    snapshotItems.clear();
+    snapshotItems = snapshotsVerified;
+
+    return snapshotItems;
+  }
+}
