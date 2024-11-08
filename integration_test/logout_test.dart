64cbdcc4f92c84363831c2e850d7b8f0ac538d5f
Gisela Martinez <giselamartinez@Giselas-MacBook-Pro.local>
2024-11-08 09:57:45 -0300

    test(Logout and Snapshots): implemented test

---
+++ integration_test/logout_test.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:ardrive/utils/widget_keys.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'app_test.dart';
+import 'dsl/dsl.dart';
+import 'utils.dart';
+import 'login_tests.dart';
+
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+
+  group('Logout Tests', () {
+    testWidgets('User can log out successfully', (WidgetTester tester) async {
+      await runPreConditionUserLoggedIn(tester);
+      await testLogoutSuccess(tester);
+    });
+  });
+}
+
+Future<void> testLogoutSuccess(WidgetTester tester) async {
+  I.see.page(driveDetailPageKey).wait(10000);
+  await I.see.profileCard().tap().wait(5000).go(tester);
+  await I.wait(1000);
+  await I.see.button('Log out').tap().wait(1000).go(tester);    
+  I.see.button('Log In');
+}
---
+++ integration_test/snapshot_test.dart
@@ -0,0 +1,39 @@
+import 'package:ardrive/utils/widget_keys.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'dsl/dsl.dart';
+import 'utils.dart';
+
+
+
+void main() {
+  group('Snapshot Tests', () {
+    testWidgets('Create a snapshot of the drive detail page', (tester) async {
+      await runPreConditionUserLoggedIn(tester);
+      await testCreateSnapshot(tester);
+    });
+  });
+}
+
+Future<void> testCreateSnapshot(WidgetTester tester) async {
+  await I.see.publicDriveButton('drive test snapshot').tap().wait(500).go(tester);
+  await I.see.newButton().tap().wait(500).go(tester);
+  await I.see.button('Advanced').tap().wait(100).go(tester);
+  await I.see.button('New Snapshot').tap().wait(500).go(tester);
+  await I.see.button('PROCEED').tap().wait(10000).go(tester);
+  await I.see.button('UPLOAD').tap().wait(10000).go(tester);
+  I.see.text('Success');
+  await I.see.button('OK').tap().wait(100).go(tester);
+}
+
+Future<void> testCreatePrivateSnapshot(WidgetTester tester) async {
+  await I.see.privateDriveButton('drive test snapshot private').tap().wait(500).go(tester);
+  await I.see.newButton().tap().wait(500).go(tester);
+  await I.see.button('Advanced').tap().wait(100).go(tester);
+  await I.see.button('New Snapshot').tap().wait(500).go(tester);
+  await I.see.button('PROCEED').tap().wait(10000).go(tester);
+  await I.see.button('UPLOAD').tap().wait(10000).go(tester);
+  I.see.text('Success');
+  await I.see.button('OK').tap().wait(100).go(tester);
+}
+
