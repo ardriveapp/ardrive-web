96c3ebd0ff873eb717b5f512a38debddba2b703a
Thiago Carvalho <32248947+thiagocarvalhodev@users.noreply.github.com>
2024-12-03 08:40:18 -0300

    Update profile_name_bloc.dart

--- lib/user/name/presentation/bloc/profile_name_bloc.dart
+++ lib/user/name/presentation/bloc/profile_name_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:ardrive/arns/domain/arns_repository.dart';
 import 'package:ardrive/authentication/ardrive_auth.dart';
 import 'package:ardrive/utils/logger.dart';
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'profile_name_event.dart';
 part 'profile_name_state.dart';
